b030d2be6e3d0a0e67ddf34a3ec00f3968e8f372
Jonathan Griffin <jdg547@raijin3.(none)>
2015-08-28 14:08:25 +1000

    Adding logic tree for b-values

--- python/parse/create_logic_tree_input.py
+++ python/parse/create_logic_tree_input.py
@@ -6,7 +6,7 @@ and a mapping to the GAR source zones
 import os, sys
 
 pathname = '/short/n74/GAR_2015/Tsunami_v2/DATA/FOR_MODEL'
-final_source_path = os.path.join(pathname, 'source_zones')
+final_source_path = os.path.join(pathname, 'source_zones_atlantic_model')
 austptha_gem_map = '/short/n74/GAR_2015/Tsunami_v2/DATA/FOR_MODEL/ancillary/aust_gem_mapping.csv'
 gem_tab_path = '/short/n74/GAR_2015/Tsunami_v2/DATA/FOR_MODEL/ancillary'
 
@@ -15,7 +15,9 @@ source_zones = os.walk(final_source_path).next()[1]
 # Create dictionary of mapping                                                               
 mapping = {}
 spareslips = {}
-sparebs = {}
+sparebs_pref = {}
+sparebs_min = {}
+sparebs_max = {}
 sparemmaxs = {}
 sparemmaxs_pref = {}
 sparemmaxs_min = {}
@@ -29,7 +31,10 @@ for line in f_in.readlines():
     if row[9] != '':
         sparemmaxs_pref[row[0]]=str(float(row[9])-0.1)
         sparemmaxs_min[row[0]]=str(float(row[9])-0.2)
-    sparebs[row[0]]=row[10].rstrip('\r\n')
+    if row[10] != '':
+        sparebs_pref[row[0]]=float(row[10].rstrip('\r\n'))
+        sparebs_min[row[0]]=float(row[10].rstrip('\r\n')) - 0.1
+        sparebs_max[row[0]]=float(row[10].rstrip('\r\n')) + 0.1
 print mapping
 f_in.close()
 
@@ -66,7 +71,9 @@ for line in f_in.readlines():
 
 # Create dictionary of GEM b values, slip-rates weighted by
 # coupling coefficient and maximum magnitudes (Mmax)
-gem_bs = {}
+gem_bs_pref = {}
+gem_bs_max = {}
+gem_bs_min = {}
 gem_mmaxs = {}
 gem_mmaxs_pref = {}
 gem_mmaxs_min = {}
@@ -93,15 +100,22 @@ for line in f_in.readlines():
     gem_mmaxs[name]=max_mag
     max_mag_min = row[5]
     gem_mmaxs_min[name] = max_mag_min
-    b_value = float(row[7])
-    gem_bs[name]=b_value
+    b_value_pref = float(row[7])
+    gem_bs_pref[name]=b_value_pref
+    b_value_min = float(row[8])
+    gem_bs_min[name]=b_value_min
+    b_value_max = float(row[9].rstrip('\r\n'))
+    gem_bs_max[name]=b_value_max
+
 f_in.close()
 
 # Combine into one dictionary
 slip_rates_min_coupling = {}
 slip_rates_max_coupling = {}
 slip_rates_pref_coupling = {}
-b_values = {}
+b_values_pref = {}
+b_values_min = {}
+b_values_max = {}
 mmaxs_pref = {}
 mmaxs = {}
 mmaxs_min = {}
@@ -111,7 +125,9 @@ for key in mapping.keys():
         slip_rates_pref_coupling[key] = float(spareslips[key])*0.5
         slip_rates_min_coupling[key] = float(spareslips[key])*0.3 #Assume min coupling of 0.3
         slip_rates_max_coupling[key] = float(spareslips[key])*0.7 # Assume max coupling of 0.7
-        b_values[key] = float(sparebs[key])
+        b_values_pref[key] = float(sparebs_pref[key])
+        b_values_min[key] = float(sparebs_min[key])
+        b_values_max[key] = float(sparebs_max[key])
         mmaxs[key] = sparemmaxs[key]
         mmaxs_pref[key] = sparemmaxs_pref[key]
         mmaxs_min[key] = sparemmaxs_min[key]
@@ -121,7 +137,9 @@ for key in mapping.keys():
             slip_pref_coupling = gem_slips_pref_coupling[gem_name]
             slip_min_coupling = gem_slips_min_coupling[gem_name]
             slip_max_coupling = gem_slips_max_coupling[gem_name]
-            b_value = gem_bs[gem_name]
+            b_value_pref = gem_bs_pref[gem_name]
+            b_value_min = gem_bs_min[gem_name]
+            b_value_max = gem_bs_max[gem_name]
             mmax = gem_mmaxs[gem_name]
             mmax_pref = gem_mmaxs_pref[gem_name]
             mmax_min = gem_mmaxs_min[gem_name]
@@ -131,7 +149,9 @@ for key in mapping.keys():
                 slip_pref_coupling = gem_slips_pref_coupling[gem_name]
                 slip_min_coupling = gem_slips_min_coupling[gem_name]
                 slip_max_coupling =gem_slips_max_coupling[gem_name]
-                b_value = gem_bs[gem_name]
+                b_value_pref = gem_bs_pref[gem_name]
+                b_value_min = gem_bs_min[gem_name]
+                b_value_max = gem_bs_max[gem_name]
                 mmax = gem_mmaxs[gem_name]
                 mmax_pref = gem_mmaxs_pref[gem_name]
                 mmax_min = gem_mmaxs_min[gem_name]
@@ -143,13 +163,20 @@ for key in mapping.keys():
         slip_rates_pref_coupling[key] = slip_pref_coupling
         slip_rates_min_coupling[key] = slip_min_coupling
         slip_rates_max_coupling[key] = slip_max_coupling
-        b_values[key] = b_value
+        b_values_pref[key] = b_value_pref
+        b_values_min[key] = b_value_min
+        b_values_max[key] = b_value_max
         mmaxs[key] = mmax
         mmaxs_pref[key] = mmax_pref
         mmaxs_min[key] = mmax_min
 
 # Write out to Hazts file
 for source_zone in source_zones:    
+    if source_zone.startswith('north_africa'):
+        if source_zone == 'north_african':
+            pass
+        else:
+            continue
 #    if source_zone != 'north_african':# and source_zone != 'cadiz' and source_zone != 'scaribbean' and source_zone != 'hellenic' and source_zone != 'antilles' and source_zone != 'cyprus' and source_zone != 'hispaniola' and source_zone != 'north_africa':  
  #       continue
     Hazts_filebasename = 'Hazts_' + source_zone + '.flt'
@@ -177,9 +204,11 @@ for source_zone in source_zones:
     lines[11] = '         1             Number of dips'
     lines[12] = '         0             Dip variations'
     lines[13] = '         1.0           Weights for dip vars'
-    lines[14] = '         1             Number of b-values'
-    lines[15] = '         %.2f          b-values' % b_values[source_zone]
-    lines[16] = '         1.0           Weights for b-values'
+    lines[14] = '         3             Number of b-values'
+    lines[15] = '         %.2f %.2f %.2f b-values' % (b_values_pref[source_zone], \
+                                                         b_values_min[source_zone],\
+                                                         b_values_max[source_zone])
+    lines[16] = '         0.6 0.2 0.2   Weights for b-values'
     lines[17] = '         3             Number of slip rates'
     lines[18] = '         %.2f %.2f %.2f         Slip rates (mm/yr)' % \
     (slip_rates_pref_coupling[source_zone], slip_rates_min_coupling[source_zone], \
@@ -196,7 +225,7 @@ for source_zone in source_zones:
     lines[27] = '         3             Number of maximum magnitudes'
     lines[28] = '         ' + mmaxs_pref[source_zone] + ' ' + mmaxs[source_zone] + ' ' + mmaxs_min[source_zone] + \
         '          Maximum magnitudes'
-    lines[29] = '         0.7 0.2 0.1          Weights for maximum mags'
+    lines[29] = '         0.6 0.3 0.1          Weights for maximum mags'
     lines[30] = '         7.8 0.1  5.0 3.0  1  1  0.0 (minmag, magstep, hxStep, hzStep, nRupArea, nRupWidth, minDepth)'
     lines[31] = '         1'
     lines[32] = '         -3.476  0.952  0.304             (rupture area consts a, b, sigma) Strasser et al 2010'
