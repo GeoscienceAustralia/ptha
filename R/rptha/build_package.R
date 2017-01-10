fbf565b5e1d7cdb886875d2e3acf697b4da7b92c
davies <none@none.com>
2017-01-10 12:23:18 +1100

    Only modify version number after build_and_check builds

--- R/rptha/build_package.R
+++ R/rptha/build_package.R
@@ -1,14 +1,14 @@
 library(devtools)
 
 # Modify the 'version'
-try({
-    description_lines = readLines('DESCRIPTION')
-    dvi = grep('Version:', description_lines)
-    new_version_number = as.numeric(strsplit(description_lines[dvi], '\\.')[[1]][3]) + 1
-    description_lines[dvi] = paste0('Version: 0.0.', new_version_number)
-    cat(description_lines, file='DESCRIPTION', sep="\n")
-    system('git commit -a -m "......increment version number"')
-})
+#try({
+#    description_lines = readLines('DESCRIPTION')
+#    dvi = grep('Version:', description_lines)
+#    new_version_number = as.numeric(strsplit(description_lines[dvi], '\\.')[[1]][3]) + 1
+#    description_lines[dvi] = paste0('Version: 0.0.', new_version_number)
+#    cat(description_lines, file='DESCRIPTION', sep="\n")
+#    system('git commit -a -m "......increment version number"')
+#})
 
 document('.')
 document('.')
