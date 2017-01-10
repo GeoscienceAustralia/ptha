cdfae7ff8da3f5eb3b61a43daf74414c5c87f1f5
davies <none@none.com>
2017-01-10 11:36:38 +1100

    more informative commit message for auto builds

--- R/rptha/build_and_check_package.R
+++ R/rptha/build_and_check_package.R
@@ -7,7 +7,7 @@ try({
     new_version_number = as.numeric(strsplit(description_lines[dvi], '\\.')[[1]][3]) + 1
     description_lines[dvi] = paste0('Version: 0.0.', new_version_number)
     cat(description_lines, file='DESCRIPTION', sep="\n")
-    system('git commit -a -m "autobuild"')
+    system('git commit -a -m "    increment version number"')
 })
 
 document('.')
