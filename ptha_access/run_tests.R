3646baae4a2a0bdf25ed1869cf7663168430894c
gareth <gareth.davies.ga.code@gmail.com>
2017-06-01 11:45:06 +1000

    add puysegur regression test

--- ptha_access/run_tests.R
+++ ptha_access/run_tests.R
@@ -8,3 +8,31 @@ gauge_netcdf_file = paste0(.GDATA_OPENDAP_BASE_LOCATION, 'SOURCE_ZONES/',
      source_zone, '/TSUNAMI_UNIT_SOURCE/', test_file) 
 
 test_sum_tsunami_unit_sources(gauge_netcdf_file)
+
+
+test_puysegur<-function(){
+    source('./get_PTHA_results.R', local=TRUE)
+   
+    puysegur = get_source_zone_events_data('puysegur')
+	model_240 = get_flow_time_series_at_hazard_point(puysegur, 240, c(55015.4, 55042.4))
+
+    max_55015 = max(model_240$flow[['55015.4']][1,,1])
+    max_55042 = max(model_240$flow[['55042.4']][1,,1])
+
+    # Values known from previous checks
+    er1 = abs(max_55015 - 0.077164)
+    er2 = abs(max_55042 - 0.052797)
+
+    m1 = which.max(model_240$flow[['55015.4']][1,,1])
+    m2 = which.max(model_240$flow[['55042.4']][1,,1])
+
+    if((er1 < 1.0e-05) & (er2 < 1.0e-05) & (m1 == 62) & (m2 == 76)){
+        print('PASS')
+    }else{
+        print('FAIL: test_puysegur giving different results')
+
+    }
+}
+
+test_puysegur()
+
