0c88c4a88c3f306cc207b435e41b37fcc19fa2f4
gareth <gareth.davies.ga.code@gmail.com>
2017-06-02 16:38:18 +1000

    add regression test for locations

--- ptha_access/test_all.R
+++ ptha_access/test_all.R
@@ -39,6 +39,26 @@ test_puysegur<-function(){
         print('FAIL: test_puysegur giving different results')
 
     }
+
+
+    # Check location info is right -- this is based on 'dput' of an earlier result
+    test_data = structure(list(lon = structure(c(129.089294433594, 129.18830871582, 
+        127.597557067871, 160.256103515625, 161.841110229492), .Dim = 5L), 
+        lat = structure(c(-8.40988159179688, -8.29211044311523, -8.10336685180664, 
+        -46.8297233581543, -44.897777557373), .Dim = 5L), elev = structure(c(-775.012756347656, 
+        -716.009460449219, -456.208404541016, -5052.998046875, -4819.00732421875
+        ), .Dim = 5L), gaugeID = structure(c(1.1, 10.1, 22.1, 55015.4, 
+        55042.4), .Dim = 5L)), .Names = c("lon", "lat", "elev", "gaugeID"
+        ), row.names = c(NA, -5L), class = "data.frame")
+
+    m1 = max(abs(model_240$location - test_data))
+    print(m1)
+    if(m1 > 1.0e-010){
+        print('FAIL -- location information haz changed')
+    }else{
+        print('PASS')
+    }
+ 
 }
 # Run the puysegur regression test
 t1 = system.time(test_puysegur())
