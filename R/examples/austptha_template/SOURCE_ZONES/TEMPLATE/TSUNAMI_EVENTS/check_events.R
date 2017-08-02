4eebf670d1e73f431048be4fc2e431f6fc67b945
gareth <gareth@gareth-P410.com>
2017-08-02 20:33:27 +1000

    Include zero slip cells in area computation

--- R/examples/austptha_template/SOURCE_ZONES/TEMPLATE/TSUNAMI_EVENTS/check_events.R
+++ R/examples/austptha_template/SOURCE_ZONES/TEMPLATE/TSUNAMI_EVENTS/check_events.R
@@ -191,11 +191,35 @@ run_checks<-function(fid_local){
             downdip_range 
             ))
 
-        event_area = unlist(lapply(unit_sources_in, 
+
+        # Area of 'non-zero' patches of the rectangular region of slip        
+        event_area_nonzero = unlist(lapply(unit_sources_in, 
             f<-function(x) sum(unit_source_statistics$length[x] * 
                 unit_source_statistics$width[x])))
-        event_mean_slip = unlist(lapply(event_slips, 
+        # Mean slip on non-zero patches of the rectangular region of slip
+        event_mean_slip_nonzero = unlist(lapply(event_slips, 
             f<-function(x) mean(x) ))
+
+        # Area of full rectangular region of slip, including effect of zero patches
+        event_area_all = unlist(mapply(
+            f<-function(strk_ind, dip_ind){
+                # Compute the width, based on least-along-strike unit-sources
+                r1 = strk_ind[1]:strk_ind[2]
+                r2 = dip_ind[1]:dip_ind[2]
+                tokeep = which(
+                    (unit_source_statistics$downdip_number %in% r2) & 
+                    (unit_source_statistics$alongstrike_number %in% r1)
+                )
+                sum(unit_source_statistics$width[tokeep]*unit_source_statistics$length[tokeep])
+            }, 
+            alongstrike_range,
+            downdip_range 
+            ))
+
+        # Mean slip, including 'zero areas
+        event_mean_slip_all = event_mean_slip_nonzero * event_area_nonzero/event_area_all
+
+        # Maximum slip
         event_max_slip = unlist(lapply(event_slips, 
             f<-function(x) max(x) ))
 
@@ -217,16 +241,24 @@ run_checks<-function(fid_local){
 
         # Plotting
         pdf(paste0('event_size_scaling_stochastic_', basename(dirname(getwd())), '.pdf'), 
-            width=10, height=10)
+            width=13, height=10)
 
-        par(mfrow=c(3,2))
+        par(mfrow=c(3,3))
 
         # Area
-        plot(mws, event_area, log='y', main='Stochastic event area with non-zero slip', 
+        plot(mws, event_area_nonzero, log='y', 
+            main='Stochastic event area with non-zero slip', 
             xlab='Mw', ylab='km^2')
         grid()
         local_scaling_law_results('area')
-       
+
+        # Area
+        plot(mws, event_area_all, log='y', 
+            main='Stochastic event area (including zero slip cells)', 
+            xlab='Mw', ylab='km^2')
+        grid()
+        local_scaling_law_results('area')
+
         # Length
         plot(mws, event_length, log='y', main='Stochastic event length', 
             xlab='Mw', ylab='km')
@@ -240,12 +272,26 @@ run_checks<-function(fid_local){
         local_scaling_law_results('width')
 
         # Mean slip
-        plot(mws, event_mean_slip, log='y', main='Stochastic event mean_slip', 
+        plot(mws, event_mean_slip_nonzero, log='y', 
+            main='Stochastic event mean_slip on non-zero slip patches', 
             xlab='Mw', ylab='km')
-        points(unique_mws, slip_from_Mw(unique_mws), t='l', col='red')
-        median_ratio = aggregate(event_mean_slip, list(mws), median)
+        points(unique_mws, slip_from_Mw(unique_mws), t='l', col='red', lty='dashed')
+        points(unique_mws, slip_from_Mw(unique_mws)*3, t='l', col='red', lty='dashed')
+        median_ratio = aggregate(event_mean_slip_nonzero, list(mws), median)
         points(median_ratio[,1], median_ratio[,2], col='red')
         grid()
+
+        # Mean slip
+        plot(mws, event_mean_slip_all, log='y', 
+            main='Stochastic event mean_slip including zero-slip patches', 
+            xlab='Mw', ylab='km')
+        points(unique_mws, slip_from_Mw(unique_mws), t='l', col='red', lty='dashed')
+        points(unique_mws, slip_from_Mw(unique_mws)*3, t='l', col='red', lty='dashed')
+        median_ratio = aggregate(event_mean_slip_all, list(mws), median)
+        points(median_ratio[,1], median_ratio[,2], col='red')
+        grid()
+
+
         # Peak slip
         plot(mws, event_max_slip, log='y', main='Stochastic event max_slip', 
             xlab='Mw', ylab='km')
@@ -256,10 +302,19 @@ run_checks<-function(fid_local){
         grid()
 
         # Peak slip / mean slip
-        plot(mws, event_max_slip/event_mean_slip, log='y', main='Stochastic event max_slip/mean_slip', 
+        plot(mws, event_max_slip/event_mean_slip_nonzero, log='y', 
+            main='Stochastic event max_slip/mean_non-zero_slip', 
             xlab='Mw', ylab='km')
         grid()
-        median_ratio = aggregate(event_max_slip/event_mean_slip, list(mws), median)
+        median_ratio = aggregate(event_max_slip/event_mean_slip_nonzero, list(mws), median)
+        points(median_ratio[,1], median_ratio[,2], col='red')
+        abline(h=3, col='red')
+
+        plot(mws, event_max_slip/event_mean_slip_all, log='y', 
+            main='Stochastic event max_slip/mean_slip_including_zeros', 
+            xlab='Mw', ylab='km')
+        grid()
+        median_ratio = aggregate(event_max_slip/event_mean_slip_all, list(mws), median)
         points(median_ratio[,1], median_ratio[,2], col='red')
         abline(h=3, col='red')
 
@@ -267,15 +322,17 @@ run_checks<-function(fid_local){
 
     }
 
+    return(environment())
+
 }
 
 # Check uniform slip
 fid_uniform = nc_open(all_uniform_eq_events_file, readunlim=FALSE)
-run_checks(fid_uniform)
+uniform_env = run_checks(fid_uniform)
 nc_close(fid_uniform)
 
 # Check stochastic slip
 fid_stochastic = nc_open(all_stochastic_eq_events_file, readunlim=FALSE)
-run_checks(fid_stochastic)
+stochastic_env = run_checks(fid_stochastic)
 nc_close(fid_stochastic)
 
