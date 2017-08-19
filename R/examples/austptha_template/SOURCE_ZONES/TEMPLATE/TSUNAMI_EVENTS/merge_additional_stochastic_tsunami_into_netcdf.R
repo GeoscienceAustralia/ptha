07446cdafcb6c014da7ece00376cd37c0f297c2a
gareth <gareth@gareth-P410.com>
2017-08-19 10:00:17 +1000

    faster netcdf writing

--- R/examples/austptha_template/SOURCE_ZONES/TEMPLATE/TSUNAMI_EVENTS/merge_additional_stochastic_tsunami_into_netcdf.R
+++ R/examples/austptha_template/SOURCE_ZONES/TEMPLATE/TSUNAMI_EVENTS/merge_additional_stochastic_tsunami_into_netcdf.R
@@ -37,26 +37,24 @@ if( length(all_R_images) > 0 ){
 
         output_nc_file = nc_open(r_env$output_file_name, readunlim=FALSE, write=TRUE)
 
-        # Put each variable, only in the contiguous part of my_events
-        ncvar_put(output_nc_file, output_nc_file$var$max_stage, r_env$gauge_event_max_stage, 
-                  start=c(r_env$my_events[1], 1), count=c(length(r_env$my_events), -1))
-        gc()
+        # Writing by looping over gauges seems faster than writing all gauges at once
+        for(j in 1:(dim(r_env$gauge_event_max_stage)[2])){
+            # Put each variable, only in the contiguous part of my_events
+            ncvar_put(output_nc_file, output_nc_file$var$max_stage, r_env$gauge_event_max_stage[,j], 
+                      start=c(r_env$my_events[1], j), count=c(length(r_env$my_events), 1))
 
-        ncvar_put(output_nc_file, output_nc_file$var$period, r_env$gauge_event_reference_period,
-                  start=c(r_env$my_events[1], 1), count=c(length(r_env$my_events), -1))
-        gc()
+            ncvar_put(output_nc_file, output_nc_file$var$period, r_env$gauge_event_reference_period[,j],
+                      start=c(r_env$my_events[1], j), count=c(length(r_env$my_events), 1))
 
-        ncvar_put(output_nc_file, output_nc_file$var$stage_range, r_env$gauge_event_peak_to_trough,
-                  start=c(r_env$my_events[1], 1), count=c(length(r_env$my_events), -1))
-        gc()
+            ncvar_put(output_nc_file, output_nc_file$var$stage_range, r_env$gauge_event_peak_to_trough[,j],
+                      start=c(r_env$my_events[1], j), count=c(length(r_env$my_events), 1))
 
-        ncvar_put(output_nc_file, output_nc_file$var$arrival_time, r_env$gauge_event_arrival_time,
-                  start=c(r_env$my_events[1], 1), count=c(length(r_env$my_events), -1))
-        gc()
+            ncvar_put(output_nc_file, output_nc_file$var$arrival_time, r_env$gauge_event_arrival_time[,j],
+                      start=c(r_env$my_events[1], j), count=c(length(r_env$my_events), 1))
 
-        ncvar_put(output_nc_file, output_nc_file$var$initial_stage, r_env$gauge_event_initial_stage,
-                  start=c(r_env$my_events[1], 1), count=c(length(r_env$my_events), -1))
-        gc()
+            ncvar_put(output_nc_file, output_nc_file$var$initial_stage, r_env$gauge_event_initial_stage[,j],
+                      start=c(r_env$my_events[1], j), count=c(length(r_env$my_events), 1))
+        }
 
         nc_close(output_nc_file)
 
