0aa0ae5b2820c6295e82f4308a6bf7b5fb81692c
gareth <gareth@gareth-P410.com>
2017-08-25 13:24:23 +1000

    new code for dart check

---
+++ R/examples/austptha_template/SOURCE_ZONES/TEMPLATE/TSUNAMI_EVENTS/check_dart_include.R
@@ -0,0 +1,78 @@
+#
+# This code can be 'included' or 'sourced' in a check_dart.R script that
+# defines:
+#  event_magnitude, event_hypocentre, gauge_ids, gauge_data, plot_durations, gauge_ylims, event_basename
+#
+# Useful to reduce code replication
+#
+
+stopifnot(exists('event_basename'))
+#
+# Uniform slip
+#
+
+check_dart_env$compare_event_with_gauge_time_series(
+    event_magnitude,
+    event_hypocentre,
+    event_start,
+    gauge_ids,
+    gauge_data,
+    plot_durations= plot_durations,
+    gauge_ylims = gauge_ylims,
+    output_dir_tag = paste0(event_basename, '_uniform'),
+    make_plot=FALSE)
+# Store NGDC comparison data
+check_dart_env$compare_event_maxima_with_NGDC(
+    event_start,
+    event_magnitude,
+    event_hypocentre,
+    output_dir_tag = paste0(event_basename, '_uniform'))
+
+#
+# Stochastic slip
+#
+
+check_dart_env$compare_event_with_gauge_time_series(
+    event_magnitude,
+    event_hypocentre,
+    event_start,
+    gauge_ids,
+    gauge_data,
+    plot_durations= plot_durations,
+    gauge_ylims = gauge_ylims,
+    output_dir_tag = paste0(event_basename, '_stochastic'),
+    make_plot=FALSE,
+    use_stochastic_slip=TRUE)
+# Store NGDC comparison data
+check_dart_env$compare_event_maxima_with_NGDC(
+    event_start,
+    event_magnitude,
+    event_hypocentre,
+    output_dir_tag = paste0(event_basename, '_stochastic'),
+    use_stochastic_slip = TRUE)
+
+#
+# Variable uniform slip
+#
+
+check_dart_env$compare_event_with_gauge_time_series(
+    event_magnitude,
+    event_hypocentre,
+    event_start,
+    gauge_ids,
+    gauge_data,
+    plot_durations= plot_durations,
+    gauge_ylims = gauge_ylims,
+    output_dir_tag = paste0(event_basename, '_variable_uniform'),
+    make_plot=FALSE,
+    use_variable_uniform=TRUE
+    )
+# Store NGDC comparison data
+check_dart_env$compare_event_maxima_with_NGDC(
+    event_start,
+    event_magnitude,
+    event_hypocentre,
+    output_dir_tag = paste0(event_basename, '_variable_uniform'),
+    use_variable_uniform_slip = TRUE)
+
+
