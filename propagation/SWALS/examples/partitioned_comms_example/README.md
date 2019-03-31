5e2675c0f44a146f49e42869b20566a00f196f83
gareth <gareth@gareth-P410.com>
2019-04-01 10:48:53 +1100

    update coarray example

---
+++ propagation/SWALS/examples/partitioned_comms_example/README.md
@@ -0,0 +1,9 @@
+This illustrates an OLD, OUTDATED parallel communication approach.
+
+It only works for a single structured grid. 
+
+For this reason it has been superseded by the "multidomain" approach.
+Nonetheless, the example is kept here in case it turns out to be useful
+again.
+
+
--- propagation/SWALS/examples/partitioned_comms_example/coarray_example.f90
+++ propagation/SWALS/examples/partitioned_comms_example/coarray_example.f90
@@ -6,39 +6,41 @@
 #   define TIMER_STOP(tname)
 #endif
 
-MODULE local_routines 
-    USE global_mod, only: dp, ip, charlen, wall_elevation
-    USE domain_mod, only: domain_type, STG, UH, VH, ELV
-    USE read_raster_mod, only: gdal_raster_dataset_type
-    USE which_mod, only: which
-    USE file_io_mod, only: read_csv_into_array
-    IMPLICIT NONE
+module local_routines 
 
-    CONTAINS 
+    use global_mod, only: dp, ip, charlen, wall_elevation
+    use domain_mod, only: domain_type, STG, UH, VH, ELV
+    use read_raster_mod, only: gdal_raster_dataset_type
+    use which_mod, only: which
+    use file_io_mod, only: read_csv_into_array
+    use stop_mod, only: generic_stop
+    implicit none
 
-    SUBROUTINE set_initial_conditions_generic(domain, input_elevation_raster, &
+    contains 
+
+    subroutine set_initial_conditions_generic(domain, input_elevation_raster, &
         input_stage_raster, hazard_points_file, skip_header,&
         adaptive_computational_extents, negative_elevation_raster)
 
-        CLASS(domain_type), TARGET, INTENT(INOUT):: domain
-        CHARACTER(len=charlen), INTENT(IN):: input_elevation_raster, &
+        class(domain_type), target, intent(inout):: domain
+        character(len=charlen), intent(in):: input_elevation_raster, &
             input_stage_raster, hazard_points_file
-        INTEGER(ip), INTENT(IN):: skip_header
-        LOGICAL, INTENT(IN):: adaptive_computational_extents, negative_elevation_raster
+        integer(ip), intent(in):: skip_header
+        logical, intent(in):: adaptive_computational_extents, negative_elevation_raster
 
-        INTEGER(ip):: i, j
-        REAL(dp), ALLOCATABLE:: x(:), y(:), xy_coords(:,:)
-        INTEGER(ip):: xl, xU, yl, yU
-        TYPE(gdal_raster_dataset_type):: elevation_data, stage_data
+        integer(ip):: i, j
+        real(dp), allocatable:: x(:), y(:), xy_coords(:,:)
+        integer(ip):: xl, xU, yl, yU
+        type(gdal_raster_dataset_type):: elevation_data, stage_data
 
         ! Make space for x/y coordinates, at which we will look-up the rasters
-        ALLOCATE(x(domain%nx(1)), y(domain%nx(1)))
+        allocate(x(domain%nx(1)), y(domain%nx(1)))
 
         x = domain%x
 
         print*, "Setting elevation ..."
 
-        CALL elevation_data%initialise(input_elevation_raster)
+        call elevation_data%initialise(input_elevation_raster)
 
         print*, '    bounding box of input elevation: ' 
         print*, '    ', elevation_data%lowerleft
@@ -52,20 +54,17 @@ MODULE local_routines
             x = x - 360.0_dp
         end where
 
-        DO j = 1, domain%nx(2)
+        do j = 1, domain%nx(2)
             y = domain%y(j)
-            CALL elevation_data%get_xy(x, y, domain%U(:,j,ELV), domain%nx(1), &
+            call elevation_data%get_xy(x, y, domain%U(:,j,ELV), domain%nx(1), &
                 bilinear=1_ip)
             if(negative_elevation_raster) domain%U(:,j,ELV) = -domain%U(:,j,ELV)
-        END DO
-        CALL elevation_data%finalise()
+        end do
+        call elevation_data%finalise()
     
-
         print*, "Setting stage ..."
-
         ! Set stage -- zero outside of initial condition file range
         domain%U(:,:,[STG,UH,VH]) = 0.0_dp
-         
         CALL stage_data%initialise(input_stage_raster)
 
         ! Do not allow periodic stage data (unlike elevation, since the latter has to be set
@@ -75,10 +74,10 @@ MODULE local_routines
 
         ! Get the x indices which are inside the stage raster 
         ! We permit this to only cover a small part of the domain
-        xl = COUNT(domain%x < stage_data%lowerleft(1)) + 1
-        xU = COUNT(domain%x < stage_data%upperright(1))
-        yl = COUNT(domain%y < stage_data%lowerleft(2)) + 1
-        yU = COUNT(domain%y < stage_data%upperright(2))
+        xl = count(domain%x < stage_data%lowerleft(1)) + 1
+        xU = count(domain%x < stage_data%upperright(1))
+        yl = count(domain%y < stage_data%lowerleft(2)) + 1
+        yU = count(domain%y < stage_data%upperright(2))
 
         print*, '    bounding box of input stage: ' 
         print*, '    ', stage_data%lowerleft
@@ -87,16 +86,16 @@ MODULE local_routines
         print*, '    yl: ', yl, ' yU: ', yU
 
         if(xl > 0 .AND. xU > 0) then
-            DO j = 1, domain%nx(2)
+            do j = 1, domain%nx(2)
                 if((domain%y(j) > stage_data%lowerleft(2)) .AND. &
                     (domain%y(j) < stage_data%upperright(2))) then
                     y(xl:xU) = domain%y(j)
                     CALL stage_data%get_xy(x(xl:xU), y(xl:xU), &
                         domain%U(xl:xU, j, STG), (xU-xl+1))
                 end if
-            END DO
+            end do
         end if
-        CALL stage_data%finalise() 
+        call stage_data%finalise() 
 
         print*, '    max stage: ', maxval(domain%U(:,:,STG))
         print*, '    min stage: ', minval(domain%U(:,:,STG))
@@ -107,7 +106,6 @@ MODULE local_routines
 
         ! Ensure stage >= elevation
         domain%U(:,:,STG) = max(domain%U(:,:,STG), domain%U(:,:,ELV)+1.0e-06)
-
 
 
         ! Setup gauges
@@ -124,7 +122,6 @@ MODULE local_routines
                 print*, '    The last  point is (lon,lat): ', domain%point_gauges%xy(1:2, domain%point_gauges%n_gauges)
                 print*, ''
             end if
-            DEALLOCATE(xy_coords)
         else
             print*, ''
             print*, 'No hazard points'
@@ -132,6 +129,10 @@ MODULE local_routines
         end if
        
         if(adaptive_computational_extents) then
+#ifdef COARRAY
+            print*, 'Cannot have adaptive computational extents with coarrays'
+            call generic_stop()
+#endif
             ! Make up some xL/xU limits to do preliminary checks on allowing that
             ! to evolve
             domain%xL = xl
@@ -140,43 +141,42 @@ MODULE local_routines
             domain%yU = yU
         end if
 
-        DEALLOCATE(x,y)
         print*, 'Initial conditions set'
         print*, ''
 
         
-    END SUBROUTINE
+    end subroutine
 
-END MODULE 
+end module 
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-PROGRAM coarray_example
+program coarray_example
 
-    USE global_mod, only: ip, dp, minimum_allowed_depth
-    USE domain_mod, only: domain_type
-    USE boundary_mod, only: flather_boundary, periodic_EW_reflective_NS
+    use global_mod, only: ip, dp, minimum_allowed_depth
+    use domain_mod, only: domain_type
+    use boundary_mod, only: flather_boundary, periodic_EW_reflective_NS
 #ifdef COARRAY
-    USE coarray_point2point_comms_mod, only: allocate_p2p_comms
+    use coarray_point2point_comms_mod, only: allocate_p2p_comms
 #endif
-    USE local_routines
-    IMPLICIT NONE
+    use local_routines
+    implicit none
 
-    INTEGER(ip) :: i, j, ti, ni, nx_ca, ny_ca
-    REAL(dp) :: last_write_time
-    TYPE(domain_type) :: domain
-    CHARACTER(charlen) :: timestepping_method, input_parameter_file
-    REAL(dp) :: approximate_writeout_frequency, final_time 
+    integer(ip) :: i, j, ti, ni, nx_ca, ny_ca
+    real(dp) :: last_write_time
+    type(domain_type) :: domain
+    character(charlen) :: timestepping_method, input_parameter_file
+    real(dp) :: approximate_writeout_frequency, final_time 
 
-    REAL(dp) :: global_ur(2), global_ll(2), global_lw(2), cfl, dx(2)
-    INTEGER(ip) :: global_nx(2)
-    INTEGER(ip) :: skip_header_hazard_points_file
-    CHARACTER(len=charlen) :: input_elevation_raster, input_stage_raster, &
+    real(dp) :: global_ur(2), global_ll(2), global_lw(2), cfl, dx(2)
+    integer(ip) :: global_nx(2)
+    integer(ip) :: skip_header_hazard_points_file
+    character(len=charlen) :: input_elevation_raster, input_stage_raster, &
         hazard_points_file, output_basedir
-    LOGICAL :: record_max_U, output_grid_timeseries, adaptive_computational_extents, &
+    logical :: record_max_u, output_grid_timeseries, adaptive_computational_extents, &
         negative_elevation_raster, ew_periodic, ns_periodic
 
-    REAL(dp) :: timestep
+    real(dp) :: timestep
     
     ! Input data
     namelist /MODELCONFIG/ &
@@ -186,6 +186,7 @@ PROGRAM coarray_example
         cfl, hazard_points_file, skip_header_hazard_points_file, record_max_U,&
         output_grid_timeseries, adaptive_computational_extents, negative_elevation_raster, &
         nx_ca, ny_ca
+
 #ifdef COARRAY
     sync all
 #endif
@@ -210,17 +211,17 @@ PROGRAM coarray_example
     print*, ''
     print*, '#### INPUT FROM MODELCONFIG ####'
     print*, ''
-    print*, 'input_elevation_raster: ', TRIM(input_elevation_raster)
-    print*, 'input_stage_raster: ', TRIM(input_stage_raster)
+    print*, 'input_elevation_raster: ', trim(input_elevation_raster)
+    print*, 'input_stage_raster: ', trim(input_stage_raster)
     print*, 'global_ll: ', global_ll
     print*, 'global_ur: ', global_ur
     print*, 'global_nx: ', global_nx
     print*, 'approximate_writeout_frequency: ', approximate_writeout_frequency
-    print*, 'output_basedir: ', TRIM(output_basedir)
+    print*, 'output_basedir: ', trim(output_basedir)
     print*, 'final_time: ', final_time
-    print*, 'timestepping_method: ', TRIM(timestepping_method)
+    print*, 'timestepping_method: ', trim(timestepping_method)
     print*, 'cfl: ', cfl
-    print*, 'hazard_points_file: ', TRIM(hazard_points_file)
+    print*, 'hazard_points_file: ', trim(hazard_points_file)
     print*, 'skip_header_hazard_points_file: ', skip_header_hazard_points_file
     print*, 'record_max_U: ', record_max_U
     print*, 'output_grid_timeseries: ', output_grid_timeseries
@@ -271,28 +272,28 @@ PROGRAM coarray_example
     domain%output_basedir = output_basedir
 
 #ifndef COARRAY    
-    CALL domain%allocate_quantities(global_lw, global_nx, global_ll)
+    call domain%allocate_quantities(global_lw, global_nx, global_ll)
 #else
     print*, 'Allocating image ', ti
     ! Allocate domain with coarrays
-    CALL domain%allocate_quantities(global_lw, global_nx, global_ll, co_size_xy = [nx_ca, ny_ca], &
+    call domain%allocate_quantities(global_lw, global_nx, global_ll, co_size_xy = [nx_ca, ny_ca], &
         ew_periodic = ew_periodic, ns_periodic = ns_periodic)
     sync all
     call allocate_p2p_comms
 #endif
 
-    CALL domain%log_outputs()
+    call domain%log_outputs()
     write(domain%logfile_unit, MODELCONFIG)
 
     ! Call local routine to set initial conditions
-    CALL set_initial_conditions_generic(domain, input_elevation_raster,&
+    call set_initial_conditions_generic(domain, input_elevation_raster,&
         input_stage_raster, hazard_points_file, skip_header_hazard_points_file,&
         adaptive_computational_extents, negative_elevation_raster)
 
 
 #ifdef COARRAY
     ! Also print information about whether boundary conditions are applied
-        write(domain%logfile_unit, *) 'ti: ', ti, 'boundary_exterior: ', domain%boundary_exterior
+    write(domain%logfile_unit, *) 'ti: ', ti, 'boundary_exterior: ', domain%boundary_exterior
 #endif
 
     timestep = domain%linear_timestep_max()
@@ -317,28 +318,25 @@ PROGRAM coarray_example
 
     TIMER_STOP('SETUP')
 
-
     ! Evolve the code
-    DO WHILE (.TRUE.)
+    do while (.true.)
 
-        IF(domain%time - last_write_time >= approximate_writeout_frequency) THEN
+        if(domain%time - last_write_time >= approximate_writeout_frequency) then
 
             last_write_time = last_write_time + approximate_writeout_frequency
 
-            CALL domain%print()
-            CALL domain%write_to_output_files(time_only = (output_grid_timeseries .EQV. .FALSE.))
-            CALL domain%write_gauge_time_series()
+            call domain%print()
+            call domain%write_to_output_files(time_only = (output_grid_timeseries .eqv. .false.))
+            call domain%write_gauge_time_series()
 
-        END IF
+        end if
 
-        IF (domain%time > final_time) THEN
-            EXIT 
-        END IF
+        if (domain%time > final_time) exit
 
         !! Example with fixed timestep
-        CALL domain%evolve_one_step(timestep=timestep)
+        call domain%evolve_one_step(timestep=timestep)
 
-        ! Evolve the active domain?
+        ! Evolve the active domain? NOT WITH COARRAYS
         domain%xL = max(domain%xL - 1, 1)
         domain%xU = min(domain%xU + 1, domain%nx(1))
         domain%yL = max(domain%yL - 1, 1)
@@ -347,19 +345,19 @@ PROGRAM coarray_example
         ! Treatment of spherical models with periodic EW conditions
         ! The BC region is within 4 cells of the boundaries (considering
         ! cells that copy-out, as well as cells that copy-in)
-        if((domain%xl <= 4).OR.(domain%xU >= domain%nx(1) - 3)) then
+        if((domain%xl <= 4).or.(domain%xU >= domain%nx(1) - 3)) then
             domain%xl = 1_ip
             domain%xU = domain%nx(1)
         end if
 
-    END DO
+    end do
 
     print*, ''
-    CALL domain%write_max_quantities()
+    call domain%write_max_quantities()
 
     ! Print timing info
-    CALL domain%timer%print(output_file_unit = domain%logfile_unit)
+    call domain%timer%print(output_file_unit = domain%logfile_unit)
 
-    CALL domain%finalise()
+    call domain%finalise()
 
-END PROGRAM
+end program
