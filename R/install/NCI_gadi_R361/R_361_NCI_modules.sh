6d1ad5bb8912878795216e1ac3b99b70522e92da
gareth <gareth@gareth-P410.com>
2020-01-24 09:13:36 +1100

    update NCI modules

--- R/install/NCI_gadi_R361/R_361_NCI_modules.sh
+++ R/install/NCI_gadi_R361/R_361_NCI_modules.sh
@@ -1,8 +1,17 @@
-# These modules can be used to compile and run 'rptha' on NCI's gadi supercomputer
-module load intel-compiler
+# These modules can be used to compile and run 'rptha'
+module load intel-compiler/2019.3.199
 module load netcdf/4.7.1
 module load proj
 module load geos
 module load R/3.6.1
-export PATH=/g/data1a/w85/tsunami/CODE/gadi/gcc_system/gdal/build/bin/:$PATH
-export LD_LIBRARY_PATH=/g/data1a/w85/tsunami/CODE/gadi/gcc_system/gdal/build/lib/:$LD_LIBRARY_PATH
+module load gdal
+
+
+## These modules can be used to compile and run 'rptha' on NCI's gadi supercomputer
+#module load intel-compiler
+#module load netcdf/4.7.1
+#module load proj
+#module load geos
+#module load R/3.6.1
+#export PATH=/g/data1a/w85/tsunami/CODE/gadi/gcc_system/gdal/build/bin/:$PATH
+#export LD_LIBRARY_PATH=/g/data1a/w85/tsunami/CODE/gadi/gcc_system/gdal/build/lib/:$LD_LIBRARY_PATH
