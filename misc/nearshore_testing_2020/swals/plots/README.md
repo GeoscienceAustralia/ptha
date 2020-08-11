19a918d75cb6a0804533deece867ee0249804bdb
gareth <gareth@gareth-P410.com>
2020-08-11 16:58:03 +1000

    remove scripts for ptha scenarios

--- misc/nearshore_testing_2020/swals/plots/README.md
+++ misc/nearshore_testing_2020/swals/plots/README.md
@@ -2,4 +2,3 @@ These codes can be used to make plots and also model-vs-data products.
 
 At the time of writing we've run published finite-fault inversions for Chile1960, Sumatra2004, Chile2010, Tohoku2011, Chile2015.
 
-We've also experimented with PTHA18 scenarios for some other events, which have corresponding scripts here, but haven't studied literature inversions for those.
--- misc/nearshore_testing_2020/swals/plots/plot_all.R
+++ misc/nearshore_testing_2020/swals/plots/plot_all.R
@@ -1,6 +1,5 @@
 ALL_INVERSIONS_FOLDERS = Sys.glob('../OUTPUTS/*/RUN*')
 
-#for(i in 1:length(all_INVERSIONS_folders)){
 parallel_job<-function(i){
     ALL_INVERSIONS_FOLDERS = Sys.glob('../OUTPUTS/*/RUN*')
     run_folder = ALL_INVERSIONS_FOLDERS[i]
@@ -11,11 +10,8 @@ parallel_job<-function(i){
     event2script = list('Chile1960'   = 'plot_southamerica1960.R',
                         'Chile2010'   = 'plot_southamerica2010.R', 
                         'Chile2015'   = 'plot_southamerica2015.R', 
-                        'Chile2014'   = 'plot_southamerica2014.R', 
                         'Sumatra2004' = 'plot_sumatra2004.R', 
-                        'Tohoku2011'  = 'plot_tohoku2011.R',
-			'Puysegur2009' = 'plot_puysegur2009.R',
-			'Solomon2007' = 'plot_solomon2007.R') 
+                        'Tohoku2011'  = 'plot_tohoku2011.R')
 
     job_command = paste0('Rscript ', event2script[[event_flag]], ' ', run_folder)
     print(job_command)
