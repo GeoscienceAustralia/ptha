3d7bfe301a304a468e78b23b2e4a27fa5e79359a
gareth <gareth@gareth-P410.com>
2020-08-12 17:20:52 +1000

    restructure so we only work with data for the nearshore2020 study

---
+++ misc/nearshore_testing_2020/swals/config.R
@@ -0,0 +1,2 @@
+NEARSHORE_TESTING_PAPER_2020_ONLY=TRUE # The value TRUE should restrict the script to content relevant to the 2020 nearshore testing paper (i.e. removing some of the in-progress PTHA18 scenario tests where we don't have published inversions)$
+
