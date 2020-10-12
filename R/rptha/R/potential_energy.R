ef5a81177f2f818d16ea48a51601836fccec78b1
gareth <gareth@gareth-P410.com>
2020-10-12 12:06:35 +1100

    add doc

--- R/rptha/R/potential_energy.R
+++ R/rptha/R/potential_energy.R
@@ -1,7 +1,8 @@
 #' Available potential energy of the sea surface.
 #'
-#' The available potential energy is computed as: \cr
-#'     integral_in_space { 0.5 * gravity * seawater_density * (sea_surface - MSL)^2 } \cr
+#' The available potential energy is computed as the following integral in space: \cr
+#'     integral [ 0.5 * gravity * seawater_density * (sea_surface - MSL)^2 ] dA \cr
+#' where dA is the 2D area element. \cr
 #' This function takes a matrix giving the sea_surface, as well as the cell areas (which should
 #' be a matrix unless the cell areas are constant). You must ensure that the sea_surface is NA
 #' in dry areas.
@@ -10,16 +11,17 @@
 #' @param cell_areas_m2 The cell area associated with each entry of sea_surface
 #' in meters**2. This can be a constant (e.g. for regular cells in Cartesian coordinates) or
 #' it can be a matrix of the same size as sea_surface
-#' @param gravity Gravitational acceleration at the earth's surface (m/s/s)
+#' @param gravity Gravitational acceleration at the earth's surface (m/s^2)
 #' @param seawater density Density of seawater in kg/m^3
 #' @param MSL the mean sea level
 #' @return The available potential energy in units of joules (kg m^2/s^2)
-#'
+#' @export
 #' @examples
 #' # Read a raster initial condition -- this is in lonlat coordinates, and is 
-#' # already NA on land
-#' chile1960_free_surface_NA_on_land = raster(
-#'     system.file('extdata/Ho_Chile1960_initial_displacement_NA_on_land.tif'))
+#' # already NA on land. It was created from this paper: https://doi.org/10.1029/2018JB016996
+#' raster_file = system.file('extdata/Ho_Chile1960_initial_displacement_NA_on_land.tif', 
+#'     package='rptha')
+#' chile1960_free_surface_NA_on_land = raster(raster_file)
 #' # Get the cell areas using the raster package's "area" function. This correctly
 #' # recognizes that the raster is lonlat
 #' cell_areas_m2 = area(chile1960_free_surface_NA_on_land) * 1e+03 * 1e+03 # convert km^2 to m^2
