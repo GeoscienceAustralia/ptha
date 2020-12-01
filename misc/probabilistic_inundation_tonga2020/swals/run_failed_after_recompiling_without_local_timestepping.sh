49c51eb662285e05ee4b0865d4de8b84b32c72ff
gareth <gareth@gareth-P410.com>
2020-12-01 15:05:15 +1100

    comment

--- misc/probabilistic_inundation_tonga2020/swals/run_failed_after_recompiling_without_local_timestepping.sh
+++ misc/probabilistic_inundation_tonga2020/swals/run_failed_after_recompiling_without_local_timestepping.sh
@@ -10,6 +10,6 @@
 source SWALS_ifort_modules.sh
 ulimit -s unlimited
 
-# This is the only run that blew-up. Try turning off local timestepping in the compilation and rerunning
+# This is the only run that blew-up. Try turning off local timestepping in the compilation and rerunning as single grids
 OMP_NUM_THREADS=48 OMP_PROC_BIND=true mpiexec -np 1 --map-by ppr:1:node:PE=48 ./model ../sources/random/scenario_initial_conditions/kermadectonga2_row_0043831_Mw_95_HS.tif 0 0.8 ptha18_tonga_MSL0.8/ptha18_random_scenarios_kermadectonga2_row_0043831_Mw_95_HS 'full' '' linear_with_manning 0.035 tonga regional few_grids > outfile.log
 #OMP_NUM_THREADS=24 OMP_PROC_BIND=true mpiexec -np 2 --map-by ppr:1:socket:PE=24 ./model ../sources/random/scenario_initial_conditions/kermadectonga2_row_0043831_Mw_95_HS.tif 0 0.8 ptha18_tonga_MSL0.8/ptha18_random_scenarios_kermadectonga2_row_0043831_Mw_95_HS 'full' '' linear_with_manning 0.035 tonga regional few_grids > outfile.log
