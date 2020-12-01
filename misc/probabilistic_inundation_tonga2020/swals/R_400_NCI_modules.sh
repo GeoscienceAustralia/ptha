f22813d0c04c83de34387a94290f117925cdf9c2
gareth <gareth@gareth-P410.com>
2020-12-01 16:57:54 +1100

    add missing file

---
+++ misc/probabilistic_inundation_tonga2020/swals/R_400_NCI_modules.sh
@@ -0,0 +1,9 @@
+# These modules can be used to compile and run 'rptha'
+module load intel-compiler/2019.3.199
+module load netcdf/4.7.1
+module load proj
+module load geos
+module load R/4.0.0
+module load gdal
+#export PATH=/g/data1a/w85/tsunami/CODE/gadi/gcc_system/gdal/build/bin/:$PATH
+#export LD_LIBRARY_PATH=/g/data1a/w85/tsunami/CODE/gadi/gcc_system/gdal/build/lib/:$LD_LIBRARY_PATH
