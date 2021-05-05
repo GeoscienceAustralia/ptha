467d3204c265a4fc724ed6139dccefda4ecbbd0d
gareth <gareth@gareth-P410.com>
2021-05-05 12:08:33 +1000

    improve test and interpolation for burn_into_grid

--- propagation/SWALS/src/util/burn_into_grid_mod.f90
+++ propagation/SWALS/src/util/burn_into_grid_mod.f90
@@ -4,7 +4,7 @@ module burn_into_grid_mod
     !! provides routines to burn these into grids. For example, this can be used to ensure that 
     !! an elevation grid includes a continuous representation of a breakwall or riverwall.
     !!
-    use global_mod, only: ip, dp, charlen
+    use global_mod, only: ip, dp, charlen, force_double
     use logging_mod
     use stop_mod
     use file_io_mod, only: read_csv_into_array
@@ -131,6 +131,7 @@ module burn_into_grid_mod
 
         real(dp) :: dx(2), xi, yi, zi, xip1, yip1, zip1, xj(1), yj(1), zj(1)
         integer(ip) :: i, i0, j0, np, j
+        real(force_double) :: n1, n2
         character(len=charlen) :: burnt
 
         if(present(burn_type)) then
@@ -162,11 +163,14 @@ module burn_into_grid_mod
                 zip1 = z(i+1)
 
                 ! Interpolate np+1 points along the line segment between points i and (i+1)
-                np = ceiling(max((xip1-xi)/dx(1), (yip1-yi)/dx(2))) + 1
+                n1 = abs(real(xip1, force_double)- real(xi, force_double))/real(dx(1), force_double)
+                n2 = abs(real(yip1, force_double)- real(yi, force_double))/real(dx(2), force_double)
+                np = ceiling(max(n1, n2)) + 1
                 do j = 0, np
                     xj = xi + (j*(xip1-xi))/np
                     yj = yi + (j*(yip1-yi))/np
                     zj = zi + (j*(zip1-zi))/np
+                    !print*, np, j, xj, yj, zj
                     call burn_xyz_into_grid(xj, yj, zj, grid, lower_left, upper_right, burnt)
                 end do
             else
@@ -212,9 +216,10 @@ module burn_into_grid_mod
         real(dp) :: expected_grid(10,10)
         real(dp) :: lx(3), ly(3), lz(3)
         integer(ip) :: i
+        real(dp), parameter :: eps = 1.0e-03_dp
 
-        lower_left = [0.0, 0.0]
-        upper_right = [10.0, 10.0]
+        lower_left = [0.0_dp, 0.0_dp]
+        upper_right = [10.0_dp, 10.0_dp]
 
         x = [1.6_dp, 2.2_dp, 3.6_dp, 8.7_dp, 9.9_dp]
         y = [0.5_dp, 1.3_dp, 2.6_dp, 3.7_dp, 4.8_dp]
@@ -268,13 +273,15 @@ module burn_into_grid_mod
 
         !
         ! Line version
+        ! (the 'eps' in the line definition helps the test to pass in both single and double precision,
+        !  otherwise round-off can shift the results).
         !
-        lx = [-1.0_dp ,  5.5_dp,  5.5_dp]
-        ly = [3.5_dp  ,  3.5_dp, 11.5_dp]
+        lx = [-1.0_dp ,  5.5_dp,  5.5_dp] + eps
+        ly = [3.5_dp  ,  3.5_dp, 11.5_dp] - eps
         lz = lx + ly
         grid = 0.0_dp
         call burn_line_into_grid(lx, ly, lz, grid, lower_left, upper_right)
-        expected_grid = 0.0
+        expected_grid = 0.0_dp
         expected_grid(1:6,4) = [4.12500000_dp, 4.93750000_dp,  5.75000000_dp, 7.37500000_dp, 8.18750000_dp, 9.00000000_dp]
         expected_grid(6,5:10) = [9.88888931_dp, 10.7777777_dp, 11.6666670_dp, 13.4444447_dp, 14.3333330_dp, 15.2222223_dp]
         if(all(abs(grid - expected_grid) < 1.0e-05_dp)) then
@@ -282,6 +289,11 @@ module burn_into_grid_mod
         else
             print*, 'FAIL'
         end if
+
+        !print*, 'grid(1:6, 4): ', grid(1:6, 4)
+        !print*, 'expected_grid(1:6, 4): ', expected_grid(1:6, 4) 
+        !print*, 'grid(6, 5:10): ', grid(6, 5:10)
+        !print*, 'expected_grid(6, 5:10): ', expected_grid(6, 5:10) 
 
     end subroutine
 
