a9f09f070d5cad9b4de10473d7521f2a6d8174de
gareth-d-ga <gareth.davies@ga.gov.au>
2021-12-16 23:06:53 +1100

    better message

--- propagation/SWALS/src/shallow_water/multidomain_mod.f90
+++ propagation/SWALS/src/shallow_water/multidomain_mod.f90
@@ -357,7 +357,8 @@ module multidomain_mod
                             if((md%domains(j)%U(i3, i2, i1) >  bignum) .or. &
                                (md%domains(j)%U(i3, i2, i1) < -bignum) .or. &
                                (md%domains(j)%U(i3, i2, i1) /= md%domains(j)%U(i3, i2, i1))) then
-                               write(log_output_unit,*) flag
+                               write(log_output_unit,*) flag, &
+                                   " (NB: overflow just after mpi-comms can result from error-stop on other images)"
                                write(log_output_unit,*) 'error-count: ', throw_error
                                write(log_output_unit,*) 'md%domains(', j,')%U(', i3, i2, i1, ')'
                                write(log_output_unit,*) md%domains(j)%U(i3, i2, i1)
