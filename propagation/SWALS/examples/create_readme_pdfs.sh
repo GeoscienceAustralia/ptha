8a6a53aa9945bc479e00e21bbb6b5152c4496f1f
gareth-d-ga <gareth.davies@ga.gov.au>
2022-08-27 17:06:36 +1000

    improve order

--- propagation/SWALS/examples/create_readme_pdfs.sh
+++ propagation/SWALS/examples/create_readme_pdfs.sh
@@ -4,4 +4,4 @@ for i in ./README.md ./*/README.md ./nthmp/*/README.md; do echo $i; cd $(dirname
 
 
 # Combine to a single pdf
-gs -dNOPAUSE -sDEVICE=pdfwrite -SOUTPUTFILE=all_test_problems.pdf -dBATCH README.pdf ./*/README.pdf ./nthmp/*/README.pdf
+gs -dNOPAUSE -sDEVICE=pdfwrite -SOUTPUTFILE=all_test_problems.pdf -dBATCH ./nthmp/README.pdf ./nthmp/*/README.pdf README.pdf ./*/README.pdf 
