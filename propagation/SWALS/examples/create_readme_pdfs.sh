384b6d0e409e88e241659e0dd0935a3bbb5756cf
gareth-d-ga <gareth.davies@ga.gov.au>
2022-08-27 15:20:43 +1000

    better figure placement

--- propagation/SWALS/examples/create_readme_pdfs.sh
+++ propagation/SWALS/examples/create_readme_pdfs.sh
@@ -1,6 +1,6 @@
 # Make pdfs
 STARTDIR=$(pwd)
-for i in ./README.md ./*/README.md ./nthmp/*/README.md; do echo $i; cd $(dirname $i); pandoc -V papersize=a4 -V geometry:margin=1in README.md -o README.pdf; cd $STARTDIR; done
+for i in ./README.md ./*/README.md ./nthmp/*/README.md; do echo $i; cd $(dirname $i); pandoc -V papersize=a4 -V geometry:margin=1in README.md -o README.pdf -f markdown-implicit_figures ; cd $STARTDIR; done
 
 
 # Combine to a single pdf
