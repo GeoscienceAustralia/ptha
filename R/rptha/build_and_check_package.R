b9f2fa4cec64adc1ada61355376c60ebdbc2862b
gareth-d-ga <gareth.davies@ga.gov.au>
2023-02-20 13:44:08 +1100

    move versioning to 0.1

--- R/rptha/build_and_check_package.R
+++ R/rptha/build_and_check_package.R
@@ -5,7 +5,7 @@ try({
     description_lines = readLines('DESCRIPTION')
     dvi = grep('Version:', description_lines)
     new_version_number = as.numeric(strsplit(description_lines[dvi], '\\.')[[1]][3]) + 1
-    description_lines[dvi] = paste0('Version: 0.0.', new_version_number)
+    description_lines[dvi] = paste0('Version: 0.1.', new_version_number)
     cat(description_lines, file='DESCRIPTION', sep="\n")
     system('git commit -a -m "......increment version number"')
 })
