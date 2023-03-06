88e8b938ed76b8ffea1213a89ac1cb91a9498666
gareth-d-ga <gareth.davies@ga.gov.au>
2023-03-07 09:37:47 +1100

    make loop parallel

--- propagation/SWALS/src/util/burn_into_grid_mod.f90
+++ propagation/SWALS/src/util/burn_into_grid_mod.f90
@@ -307,6 +307,8 @@ module burn_into_grid_mod
            upper_right(1) < min_x .or. lower_left(1) > max_x) return
 
         ! Search each grid point
+        !$OMP PARALLEL DO DEFAULT(PRIVATE) &
+        !$OMP SHARED(poly_x, poly_y, poly_value, grid, upper_right, lower_left, burnt, min_y, min_x, max_y, max_x)
         do j = 1, size(grid, 2)
             ! Get y coord
             y = lower_left(2) + ((j - 0.5_dp)/size(grid, 2))*(upper_right(2) - lower_left(2))
@@ -341,6 +343,7 @@ module burn_into_grid_mod
                 end if
             end do
         end do
+        !$OMP END PARALLEL DO
 
     end subroutine
 
