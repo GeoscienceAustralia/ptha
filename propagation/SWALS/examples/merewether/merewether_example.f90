ecaf15687000c741020551047c3e8f127df26ad9
gareth-d-ga <gareth.davies@ga.gov.au>
2023-03-07 11:25:32 +1100

    slight cleanup

--- propagation/SWALS/examples/merewether/merewether_example.f90
+++ propagation/SWALS/examples/merewether/merewether_example.f90
@@ -1,4 +1,4 @@
-module local_routines 
+module local_routines
     !!
     !! Setup the Merewether problem.
     !!
@@ -6,11 +6,10 @@ module local_routines
     use global_mod, only: dp, ip, force_double, charlen, pi
     use domain_mod, only: domain_type, STG, UH, VH, ELV
     use read_raster_mod, only: multi_raster_type
-    use which_mod, only: which
-    use ragged_array_mod, only: ragged_array_2d_ip_type
-    use file_io_mod, only: read_character_file, read_csv_into_array
-    use points_in_poly_mod, only: points_in_poly
     use burn_into_grid_mod, only: polygons_values_type
+    use ragged_array_mod, only: ragged_array_2d_ip_type
+    use which_mod, only: which
+    use file_io_mod, only: read_character_file
     use logging_mod, only: log_output_unit
     use iso_c_binding, only: c_f_pointer, c_loc
     implicit none
@@ -21,12 +20,12 @@ module local_routines
     real(force_double) :: rain_rate =  real(19.7_dp, force_double)/(pi * rain_radius**2)
     integer(ip) :: num_input_discharge_cells
 
-    contains 
+    contains
 
-    !
-    ! Main setup routine
-    !
-    subroutine set_initial_conditions_merewether(domain)            
+    subroutine set_initial_conditions_merewether(domain)
+        !
+        ! Initialise the domain elevation, stage, flux, manning roughness, and forcing term
+        !
         class(domain_type), target, intent(inout):: domain
 
         integer(ip):: i, j, k, house_file_unit
@@ -45,7 +44,7 @@ module local_routines
         character(len=charlen), allocatable:: house_filenames(:)
         real(dp), allocatable :: house_heights(:)
         type(polygons_values_type) :: house_pvt, road_pvt
-        
+
         ! For inflow boundary
         type(ragged_array_2d_ip_type), pointer :: rainfall_region_indices
 
@@ -67,31 +66,24 @@ module local_routines
             call elevation_data%get_xy(x, y, domain%U(:,j,ELV), domain%nx(1), bilinear=0_ip)
         end do
         call elevation_data%finalise
-
         write(log_output_unit, *) 'Elevation range: ', minval(domain%U(:,:,ELV)), maxval(domain%U(:,:,ELV))
 
-        ! Add house heights to DEM
+        ! Add house heights to elevation
         open(newunit = house_file_unit, file='houses_filenames.txt') ! Names of house polygons
         call read_character_file(house_file_unit, house_filenames, "(A)")
         close(house_file_unit)
         allocate(house_heights(size(house_filenames)))
-        house_heights = house_height ! Height in each house
+        house_heights = house_height ! Height of each house above the DEM
         call house_pvt%setup(house_filenames, house_heights) ! Type for setting grid values in polygons
         call house_pvt%burn_into_grid(domain%U(:,:,ELV), domain%lower_left, domain%lower_left+domain%lw, burn_type='add')
 
         ! Transmissive outflow
-        ! We prevent mass leaking by using walls for 2 boundaries
+        ! Prevent mass leaking by using walls for south, west boundaries
         domain%U(:, 1:2, ELV) = 100.0_dp
         domain%U(1:2, :, ELV) = 100.0_dp
-        ! We use a flat boundary on the east and north so that the transmissive BC is well-behaved
+        ! Flat boundary on the east and north so that the transmissive BC is well-behaved
         domain%U(domain%nx(1), :, ELV) = domain%U(domain%nx(1)-1, :, ELV)
-        domain%U(:, domain%nx(2), ELV) = domain%U(:, domain%nx(2)-1, ELV) 
-
-        ! Set friction
-        domain%manning_squared = friction_other * friction_other ! Background value
-        ! Another friction value for points on the road
-        call road_pvt%setup([character(len=charlen)::'Road/RoadPolygon.csv'], [friction_road*friction_road])
-        call road_pvt%burn_into_grid(domain%manning_squared, domain%lower_left, domain%lower_left+domain%lw)
+        domain%U(:, domain%nx(2), ELV) = domain%U(:, domain%nx(2)-1, ELV)
 
         ! Ensure stage >= elevation
         domain%U(:,:,STG) = max(domain%U(:,:,STG), domain%U(:,:,ELV) + 1.0e-08_dp)
@@ -99,34 +91,38 @@ module local_routines
         write(log_output_unit, *) 'Elevation range: ', minval(domain%U(:,:,ELV)), maxval(domain%U(:,:,ELV))
         write(log_output_unit, *) 'Stage range: ', minval(domain%U(:,:,STG)), maxval(domain%U(:,:,STG))
 
-        ! Figure out indices that are inside the rainfall forcing region.
+        ! Set friction
+        domain%manning_squared = friction_other * friction_other ! Background value (generic urban roughness)
+        call road_pvt%setup([character(len=charlen)::'Road/RoadPolygon.csv'], [friction_road*friction_road]) ! Low road roughness
+        call road_pvt%burn_into_grid(domain%manning_squared, domain%lower_left, domain%lower_left+domain%lw)
+
+        ! Find indices inside the rainfall forcing region.
         allocate(rainfall_region_indices)
         allocate(rainfall_region_indices%i2(domain%nx(2)))
         num_input_discharge_cells = 0
         do j = 1, domain%nx(2)
-            ! Find cells in this row that are within the rain circle
-            call which( (domain%x - rain_centre(1))**2 < rain_radius**2 - (domain%y(j) - rain_centre(2))**2, &
+            ! Cell indices on this row within the rain circle
+            call which( (domain%x - rain_centre(1))**2 + (domain%y(j) - rain_centre(2))**2 < rain_radius**2, &
                 rainfall_region_indices%i2(j)%i1)
 
-            ! For this routine we cross-check conservation by counting the number of input discharge cells and
-            ! doing a separate mass balance. This assumes 1 domain (only)
+            ! To cross-check conservation, count input discharge cells (allows separate mass balance)
             num_input_discharge_cells = num_input_discharge_cells + size(rainfall_region_indices%i2(j)%i1)
         end do
 
         ! Add rainfall to the domain
         domain%forcing_subroutine => apply_rainfall_forcing
-        domain%forcing_context_cptr = c_loc(rainfall_region_indices)
-        call domain%store_forcing()
+        domain%forcing_context_cptr = c_loc(rainfall_region_indices) ! Data used in domain%forcing_subroutine
+        call domain%store_forcing() ! Ensure the above forcings are stored (even if we append other forcing terms)
 
         deallocate(x,y, house_filenames)
 
     end subroutine
 
-    !
-    ! This is the discharge source term, called every time-step. It's like rainfall 
-    ! in a "circle"
-    !
     subroutine apply_rainfall_forcing(domain, dt)
+        !
+        ! This is the discharge source term, called every time-step. It's like rainfall
+        ! in a "circle"
+        !
         type(domain_type), intent(inout) :: domain
         real(dp), intent(in) :: dt
 
@@ -143,7 +139,7 @@ module local_routines
             if(size(rainfall_region_indices%i2(j)%i1) > 0) then
                 ! Rainfall at cells within a circle of radius "rain_radius" about "rain_centre"
                 domain%U(    rainfall_region_indices%i2(j)%i1, j,STG) = rain_rate * dt + &
-                    domain%U(rainfall_region_indices%i2(j)%i1, j,STG) 
+                    domain%U(rainfall_region_indices%i2(j)%i1, j,STG)
             end if
         end do
         !$OMP END DO
@@ -152,14 +148,14 @@ module local_routines
     end subroutine
 
 
-end module 
+end module
 
 !@!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 program merewether
     !! Urban flooding test case in Merewether from Australian Rainfall and Runoff.
-    !! Smith, G. & Wasko, C. Revision Project 15: Two Dimensional Simulations In 
-    !! Urban Areas - Representation of Buildings in 2D Numerical Flood Models Australian 
+    !! Smith, G. & Wasko, C. Revision Project 15: Two Dimensional Simulations In
+    !! Urban Areas - Representation of Buildings in 2D Numerical Flood Models Australian
     !! Rainfall and Runoff, Engineers Australia, 2012
 
     use global_mod, only: ip, dp, minimum_allowed_depth, default_nonlinear_timestepping_method
@@ -179,7 +175,7 @@ program merewether
     ! Approx timestep between outputs
     real(dp), parameter :: approximate_writeout_frequency = 60.0_dp
     real(dp), parameter :: final_time = 900.0_dp
-    
+
     ! Length/width
     real(dp), parameter, dimension(2):: global_lw = [320.0_dp, 415.0_dp]
     ! Lower-left corner coordinate
@@ -189,8 +185,8 @@ program merewether
     ! Track mass
     real(force_double) :: volume_added
 
-    ! Single domain model 
-    allocate(md%domains(1)) 
+    ! Single domain model
+    allocate(md%domains(1))
 
     md%domains(1)%lw = global_lw
     md%domains(1)%nx = global_nx
@@ -205,17 +201,17 @@ program merewether
         call set_initial_conditions_merewether(md%domains(j))
 
         if(.not. all(md%domains(j)%is_nesting_boundary)) then
-            ! Allow waves to propagate out of the domain 
+            ! Allow waves to propagate out of the domain
             md%domains(j)%boundary_subroutine => transmissive_boundary
-            ! Note we adjusted the topography 
+            ! Note we adjusted the topography
             ! (inside set_initial_conditions_merewether) to ensure that
             ! the outflow is restricted to the downstream edge, and that the
-            ! transmissive boundary is well behaved (boundary elevation 
+            ! transmissive boundary is well behaved (boundary elevation
             ! equal to that just inside the boundary).
         end if
     end do
 
-    call md%make_initial_conditions_consistent() 
+    call md%make_initial_conditions_consistent()
 
     ! Evolve the model
     do while (.TRUE.)
