36429245999a749c6a3b4896acfae9000d88588a
gareth-d-ga <gareth.davies@ga.gov.au>
2023-06-02 08:56:45 +1000

    remove unrequired files

--- misc/SW_WA_2021_2024/Greater_Perth/analysis/probabilistic_inundation/make_vrt_outerrisesunda_mean_and_variance.sh
+++
@@ -1,7 +0,0 @@
-# Run this from INSIDE the folders containing the tifs
-export RESVALS='0.000102880658436 0.000102880658436'
-
-for namestart in unsegmented_HS; do 
-    gdalbuildvrt -tr $RESVALS $namestart'_exrate_all.vrt' $namestart'_domain_'*domain__exceedance*.tif;
-    gdalbuildvrt -tr $RESVALS $namestart'_exrate_variance_all.vrt' $namestart'_domain_'*domain__variance*.tif;
-    done
--- misc/SW_WA_2021_2024/Greater_Perth/analysis/probabilistic_inundation/make_vrt_sunda2_mean_and_variance.sh
+++
@@ -1,7 +0,0 @@
-# Run this from INSIDE the folders containing the tifs
-export RESVALS='0.000102880658436 0.000102880658436'
-
-for namestart in unsegmented_HS sumatra_segment_HS java_segment_HS andaman_segment_HS arakan_segment_HS; do 
-    gdalbuildvrt -tr $RESVALS $namestart'_exrate_all.vrt' $namestart'_domain_'*domain__exceedance*.tif;
-    gdalbuildvrt -tr $RESVALS $namestart'_exrate_variance_all.vrt' $namestart'_domain_'*domain__variance*.tif;
-    done
