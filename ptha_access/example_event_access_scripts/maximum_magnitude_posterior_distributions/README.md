d5ba8adb31e8e15a8f26c2169f699bd4a17ac1cb
gareth-d-ga <gareth.davies@ga.gov.au>
2024-05-16 17:06:44 +1000

    add Mw-max info

---
+++ ptha_access/example_event_access_scripts/maximum_magnitude_posterior_distributions/README.md
@@ -0,0 +1,11 @@
+# Uncertainties in Maximum magnitude on each source zone
+
+The maximum earthquake magnitude on any given source zone is often of interest. Its value is usually uncertain and often controversial. We know that the smallest plausible value is greater than the largest historically observed earthquake. But this is often a very weak constraint, and history shows that the real maximum magnitude could be much greater. 
+
+For example, prior to the 2004 Sumatra Andaman earthquake (Mw 9.2), the largest historical earthquake in that region was smaller than Mw 8 [Satake and Atwater 2007](https://doi.org/10.1146/annurev.earth.35.031306.140302). On the Cascadia subduction zone, the largest historical earthquake is currently well below Mw 8, yet sedimentary evidence suggests events nearing Mw 9 may occur every few hundred years. Even when the largest known events are quite big, there is no guarantee that they will reflect the maximum magnitude, which is potentially larger still. 
+
+PTHA18 attempts to reflect uncertainties in maximum magnitudes (and other source-zone parameters) by using multiple alternative models with Bayesian weights. Details are in [this paper](https://doi.org/10.1007/s00024-019-02299-w) and [this report](http://dx.doi.org/10.11636/Record.2018.041). Our lower limits on Mw-max are based on historical events (or available paleo interpretations), while our upper limits on Mw-max are inferred from scaling relations (but capped to a limit of 9.6).
+
+The approach leads to a (posterior) probability distribution for the maximum magnitude on each source zone. On source-zones with optional segmentation there are different distributions for the unsegmented model, and each segment.
+
+
---
+++ ptha_access/example_event_access_scripts/maximum_magnitude_posterior_distributions/compute_Mw_max_uncertainty.R
@@ -0,0 +1,48 @@
+# Get the detailed source-zone info
+ptha18_detailed = new.env()
+source('../../get_detailed_PTHA18_source_zone_info.R', chdir=TRUE, local=ptha18_detailed)
+
+source_zones = names(ptha18_detailed$crs_data$source_envs)
+
+
+#source_zone = source_zones[1]
+
+get_source_zone_Mw_max_CDF<-function(source_zone){
+
+    # Object with data for the source-zone
+    se = ptha18_detailed$crs_data$source_envs[[source_zone]]
+
+    # Quick exit for defunct sources (these have rate=0)
+    if(all(se$event_rates == 0) ) return(NULL)
+
+    # Get the rate curves
+    all_rate_curves = se$mw_rate_function(NA, return_all_logic_tree_branches=TRUE)
+
+    # Get the Max-max CDF
+    var = 'Mw_max'
+    vars = sort(unique(all_rate_curves$all_par[[var]]))
+    vars_cdf = sapply(vars, f<-function(x){
+        sum(all_rate_curves$all_par_prob*(all_rate_curves$all_par[[var]] <= x))} )
+
+    # Invert at the target percentiles
+    target_percentiles = c(0, 0.01, 0.05, seq(0.1, 0.9, by=0.1), 0.95, 0.99, 1)
+    Mw_max_at_target_percentiles = approx(vars_cdf, vars, xout=target_percentiles, method='constant', f=0, rule=2)
+
+    # Output as a data.frame
+    output_df = as.list(Mw_max_at_target_percentiles$y)
+    names(output_df) = paste0('p_', as.character(Mw_max_at_target_percentiles$x))
+    output_df = as.data.frame(output_df)
+
+    output_df = cbind(data.frame(source_zone = source_zone, is_a_segment=se$is_a_segment), output_df)
+    return(output_df)
+}
+
+all_results = lapply(source_zones, get_source_zone_Mw_max_CDF)
+names(all_results) = source_zones
+
+result_length = unlist(lapply(all_results, length))
+keep_results = all_results[result_length > 0]
+
+output_df = do.call(rbind, keep_results)
+
+write.csv(output_df, file='Mw_max_percentiles_from_PTHA18.csv', row.names=FALSE)
