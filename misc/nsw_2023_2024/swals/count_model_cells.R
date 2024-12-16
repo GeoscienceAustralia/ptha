40fa10fe579879aed8f5209f9aec700fa3427494
gareth-d-ga <gareth.davies@ga.gov.au>
2024-12-17 10:41:10 +1100

    update cell counting code in NSW model

--- misc/nsw_2023_2024/swals/count_model_cells.R
+++ misc/nsw_2023_2024/swals/count_model_cells.R
@@ -1,23 +1,50 @@
-regular_files = Sys.glob('OUTPUTS/run_kt43731_12h_final_NNL4_1arcminoffshore-full-ambient_sea_level_1.1/RUN_20241112_155633335/max_stage_domain_*.tif')
-convergence_files = Sys.glob('OUTPUTS/run_kt43731_12h_final_NNL4_CONVERGENCE-full-ambient_sea_level_1.1/RUN_20241112_173726773/max_stage_domain_*.tif')
+# Get the SWALS plot scripts
+file_home = '/home/gareth/Code_Experiments/fortran/Structured_shallow_water/plot.R'
+file_nci = '/g/data/w85/tsunami/CODE/gadi/ptha/propagation/SWALS/plot.R'
+source(ifelse(file.exists(file_home), file_home, file_nci))
 
-# Get total number of cells and non-NA cells
-count_cells=function(x){
-    library(terra)
-    r1 = rast(x)
-    total= prod(dim(r1)) # Beware this misses most halos from domain 1 (since it was merged to make the tif)
-    total_nonNA = sum(!is.na(as.matrix(r1)))
-    c(total, total_nonNA)
-    }
-regular_cell_counts = lapply(regular_files, count_cells)
-regular_total_cells = sum(unlist(lapply(regular_cell_counts, function(x) x[1]))) # This doesn't capture halos on the global domains
-regular_total_cells_noNA = sum(unlist(lapply(regular_cell_counts, function(x) x[2])))
-print(paste0('Regular model uses ', regular_total_cells_noNA, ' cells in priority domain areas'))
-print(paste0('Regular model uses more than ', regular_total_cells, ' cells in total (this calculation misses halos on domain 1)'))
+#
+# Cell counts from tif files on home machine
+#
+if(FALSE){
+    # Cell counts for all tifs
+    print('# Regular model')
+    regular_files = Sys.glob('OUTPUTS/run_kt43731_12h_final_NNL4_1arcminoffshore-full-ambient_sea_level_1.1/RUN_20241112_155633335/elevation0_domain_*.tif')
+    count_cells_all_files(regular_files)
+# [1] "# Regular model"
+# [1] "Total of 267433452 cells priority domain areas that did not have NA raster values"
+# [1] "Total of 283608244 cells, except this calculation misses halos on merged domains"
+# [1] "  Ratio: 0.942967835589434"
+    
+    print('')
+    print('# CONVERGENCE MODEL')
+    convergence_files = Sys.glob('OUTPUTS/run_kt43731_12h_final_NNL4_CONVERGENCE-full-ambient_sea_level_1.1/RUN_20241112_173726773/elevation0_domain_*.tif')
+    count_cells_all_files(convergence_files)
+# [1] ""
+# [1] "# CONVERGENCE MODEL"
+# [1] "Total of 1457837568 cells priority domain areas that did not have NA raster values"
+# [1] "Total of 1527715964 cells, except this calculation misses halos on merged domains"
+# [1] "  Ratio: 0.954259562872513"
+}
 
-convergence_cell_counts = lapply(convergence_files, count_cells)
-convergence_total_cells = sum(unlist(lapply(convergence_cell_counts, function(x) x[1]))) # This doesn't capture halos on the global domains
-convergence_total_cells_noNA = sum(unlist(lapply(convergence_cell_counts, function(x) x[2])))
-print(paste0('convergence test model uses ', convergence_total_cells_noNA, ' cells in priority domain areas'))
-print(paste0('convergence test model uses more than ', convergence_total_cells, ' cells in total (this calculation misses halos on domain 1)'))
+#
+# Cell counts from netcdf files on NCI -- these are better.
+#
+if(FALSE){
+    #
+    # Cell counts for netcdf, more accurate than above
+    #
 
+    all_netcdf_files = Sys.glob('OUTPUTS/run_Chile2010_Lorito11_1arcminoffshore-full-ambient_sea_level_0.0/RUN_20231129_183023159/*/Grid*.nc')
+    tmp = count_cells_all_files(all_netcdf_files)
+# [1] "Total of 267433452 cells in priority domain areas"
+# [1] "Total of 284580460 cells in total"
+# [1] "  Ratio: 0.939746362065758"
+
+    all_nc_files = Sys.glob('OUTPUTS/run_kt43731_12h_final_NNL4_CONVERGENCE-full-ambient_sea_level_1.1/RUN_20241112_173726773/*/Grid*.nc')
+    tmp2 = count_cells_all_files(all_nc_files)
+# [1] "Total of 1457837568 cells in priority domain areas"
+# [1] "Total of 1533668044 cells in total"
+# [1] "  Ratio: 0.950556134818964"
+
+}
