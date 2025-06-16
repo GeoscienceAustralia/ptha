d94c7b43a72aef624b48df1f1b92cf20c2f54091
matt-mac-ga <matthew.macaulay@ga.gov.au>
2025-06-16 12:32:39 +1000

    tidy bounds in threshold_given_exrate

--- misc/gladstone_2024_2025/analysis/probabilistic_inundation/threshold_given_exrate/tidy_bounds_in_thresholds_at_exceedance_rate_of_epistemic_uncertainty_percentile.R
+++ misc/gladstone_2024_2025/analysis/probabilistic_inundation/threshold_given_exrate/tidy_bounds_in_thresholds_at_exceedance_rate_of_epistemic_uncertainty_percentile.R
@@ -5,16 +5,14 @@
 # should be missing (i.e. reflecting no flow at the given exceedance-rate/percentile). 
 #
 # This script creates new rasters that are easier to interpret near the lower
-# bounds.
+# bounds. For the minimum stage, the maximum values (approaching zero from below) are tidied.
 library(terra)
 library(parallel)
 
 raster_data_list = commandArgs(trailing=TRUE)[1]
-if(raster_data_list == "") raster_data_list = '84pc'
+if(is.na(raster_data_list)) raster_data_list = '84pc'
 
 if(raster_data_list == '50pc'){
-
-    stop('FIXME: These have not yet been updated from the NSW case')
     #
     # INPUTS, 50th percentile case
     #
@@ -30,9 +28,15 @@ if(raster_data_list == '50pc'){
         # Max-stage rasters
         max_stage = list(
             rasters = Sys.glob('ptha_highres_domains_max_stage_at_epistemic_uncertainty_50pc/*.tif'),
-            NA_below = 1.1015, # No lower limit required #0.6 + 0.0015, # Lower limit was 0.6. Compare to root-finding tolerance of 1e-03
+            NA_below = 0.015, # No lower limit required #0.6 + 0.0015, # Lower limit was 0.6. Compare to root-finding tolerance of 1e-03
             output_dir = './ptha_max_stage_1in250_50pc',
             output_vrt = 'ptha_max_stage_1in250_50pc.vrt'),
+
+        min_stage = list(
+            rasters = Sys.glob('ptha_highres_domains_min_stage_at_epistemic_uncertainty_percentile_0.84_exrate_0.0004/*.tif'),
+            NA_above = -0.0015, # Compare to root-finding tolerance of 1e-03
+            output_dir = './ptha_min_stage_1in2500_84pc',
+            output_vrt = 'ptha_min_stage_1in2500_84pc.vrt'),
 
         # Max-flux rasters
         max_flux = list(
@@ -69,6 +73,12 @@ if(raster_data_list == '50pc'){
             output_dir = './ptha_max_stage_1in2500_84pc',
             output_vrt = 'ptha_max_stage_1in2500_84pc.vrt'),
 
+        min_stage = list(
+            rasters = Sys.glob('ptha_highres_domains_min_stage_at_epistemic_uncertainty_percentile_0.84_exrate_0.0004/*.tif'),
+            NA_above = -0.0015, # Compare to root-finding tolerance of 1e-03
+            output_dir = './ptha_min_stage_1in2500_84pc',
+            output_vrt = 'ptha_min_stage_1in2500_84pc.vrt'),
+
         # Max-flux rasters
         max_flux = list(
             rasters = Sys.glob('ptha_highres_domains_max_flux_at_epistemic_uncertainty_percentile_0.84_exrate_0.0004/*.tif'),
@@ -83,7 +93,7 @@ if(raster_data_list == '50pc'){
             output_dir = './ptha_max_speed_1in2500_84pc',
             output_vrt = 'ptha_max_speed_1in2500_84pc.vrt')
 
-        )
+    )
 
 }else{
     stop(paste0('unknown input ', raster_data_list))
@@ -98,21 +108,23 @@ for(varname in names(rasters_to_process)){
 
     rasters = rasters_to_process[[varname]]$rasters
     NA_below = rasters_to_process[[varname]]$NA_below
+    NA_above = rasters_to_process[[varname]]$NA_above
     output_dir = rasters_to_process[[varname]]$output_dir
     output_vrt = rasters_to_process[[varname]]$output_vrt
-
+    
     dir.create(output_dir, recursive=TRUE, showWarnings=FALSE)        
 
-    threshold_raster<-function(filename, output_dir, NA_below){
+    threshold_raster<-function(filename, output_dir, NA_below, NA_above){
         r1 = rast(filename)
-        output_file = paste0(output_dir, '/', basename(filename))        
-        r1[r1 < NA_below] = NA
+        output_file = paste0(output_dir, '/', basename(filename))
+        if (!is.null(NA_below)) r1[r1 < NA_below] = NA
+        if (!is.null(NA_above)) r1[r1 > NA_above] = NA
         writeRaster(r1, file=output_file, gdal=c('COMPRESS=DEFLATE'), overwrite=TRUE)
         rm(r1); gc()
         return(output_file)
     }
 
-    mclapply(rasters, threshold_raster, output_dir=output_dir, NA_below=NA_below, mc.cores=MC_CORES)
+    mclapply(rasters, threshold_raster, output_dir=output_dir, NA_below=NA_below, NA_above=NA_above, mc.cores=MC_CORES)
 
     # Make a vrt file
     mydir = getwd()
