cbebb6b211fdea6f67daab02cb49837967e964b8
gareth-d-ga <gareth.davies@ga.gov.au>
2025-10-09 12:37:54 +1100

    add other solvers and increase error tolerance so that coarse cliffs passes

---
+++ propagation/SWALS/examples/dispersive/shoaling_variable_seabed/run_and_plot.R
@@ -0,0 +1,167 @@
+#
+# Run and plot the "Shoaling with a variable seabed' problem 
+#
+numerical_schemes = c('midpoint', 'rk2', 'leapfrog_nonlinear', 'cliffs') #c('midpoint', 'leapfrog_nonlinear', 'rk2', 'cliffs')
+resolutions = c(0.04, 0.01) #c(0.04, 0.02, 0.01) #c(0.1, 0.05)
+
+data_store = list()
+for(nm in numerical_schemes){
+
+    for(long_dimension in c('y', 'x')){
+    
+        # Run each resolution
+        for(res in resolutions){
+            run_command = paste0('OMP_NUM_THREADS=6 OMP_PROC_BIND=true ./shoaling_variable_seabed ', nm, ' ', res, ' ', long_dimension)
+            system(run_command)
+        }
+
+    }
+
+    # Find each run
+    matching_dirs = Sys.glob(paste0('OUTPUTS/', nm, '*/RUN*'))
+    matching_dir_labels = basename(dirname(matching_dirs))
+
+    # Get a good order
+    o1 = order(as.numeric(sapply(matching_dir_labels, 
+                                function(x){tmp = strsplit(x, '_')[[1]]; tmp[length(tmp)]}
+            )), decreasing=TRUE)
+    matching_dirs = matching_dirs[o1]
+    matching_dir_labels = matching_dir_labels[o1]
+
+    data_store[[nm]] = vector(mode='list', length=length(matching_dirs))
+    names(data_store[[nm]]) = matching_dir_labels
+
+    #
+    # Plot like second panel of Figure 10 in Madsen et al (2008)
+    #
+    png(paste0('Solution_', nm, '.png'), width=8, height=4, units='in', res=200)
+    library(ncdf4)
+    for(i in 1:length(matching_dirs)){
+        # Read outputs
+        nc_file = Sys.glob(paste0(matching_dirs[i], '/RUN*/Grid*.nc'))
+        fid = nc_open(nc_file)
+        stg = ncvar_get(fid, 'stage')
+        x = ncvar_get(fid, 'x')
+        y = ncvar_get(fid, 'y')
+        time = ncvar_get(fid, 'time')
+        max_stage = ncvar_get(fid, 'max_stage')
+        min_stage = ncvar_get(fid, 'max_stage')
+        min_stage = ncvar_get(fid, 'min_stage')
+        elev = ncvar_get(fid, 'elevation0')
+        nc_close(fid)
+
+        # Plot depends on whether the long dimension is 'y' or 'x'
+        if(grepl('long_dimension_y', matching_dirs[i])){
+            k = which(elev[3,] < max(elev)-1e-03) # Used to avoid walls (1.0 m)
+            x_k = y[k]
+            max_stg_k = max_stage[3,k]
+            min_stg_k = min_stage[3,k]
+        }else if(grepl('long_dimension_x', matching_dirs[i])){
+            k = which(elev[,3] < max(elev)-1e-03) # Used to avoid walls (1.0 m)
+            x_k = x[k]
+            max_stg_k = max_stage[k,3]
+            min_stg_k = min_stage[k,3]
+        }else{
+            stop('unknown long_dimension')
+        }
+
+        if(i == 1){
+            plot(x_k, max_stg_k, t='l', ylim=c(-0.25, 0.15), col=i, 
+                main='Water surface envelope', xlab='x', ylab='Stage (m)')
+            grid()
+        }else{
+            points(x_k, max_stg_k, t='l', col=i)
+        }
+        points(x_k, min_stg_k, t='l', col=i)
+
+        data_store[[nm]][[matching_dir_labels[i]]] = data.frame(x_k = x_k, max_stg_k = max_stg_k, min_stg_k = min_stg_k)
+
+    }
+
+    # Data digitized from 2 figures, so 2 parts
+    d1 = read.csv('solutions_up_to_30m_A2.csv')
+    d2 = read.csv('solutions_beyond_30m.csv')
+    d1 = rbind(d1, d2)
+    points(d1[,1], d1[,2], t='l', lty='dotted')
+    points(d1[,1], d1[,3], t='l', lty='dotted')
+
+    legend('bottomright', 
+        c(paste0(matching_dir_labels, 'm'), 'Whispers3D'), 
+        col=c(1:length(matching_dirs), 1), 
+        lty=c(rep('solid', length(matching_dirs)), 'dotted'), 
+        pch=NA,
+        bty='n')
+    dev.off()
+
+    #
+    # Check that the numerical solutions computed along x/y are consistent with each other
+    #
+    for(res in resolutions){
+        # Find runs that are the same except for their long_dimension
+        ii = which(endsWith(names(data_store[[nm]]), paste0('_', res))); stopifnot(length(ii) == 2)
+        # Should have minimal solution change when adjusting x/y dim
+        err_stat = max(abs(data_store[[nm]][[ ii[1] ]] - data_store[[nm]][[ ii[2] ]]))
+        if(all(err_stat < 1e-06)){
+            print('PASS')
+        }else{
+            print(paste0('FAIL (long-x vs long-y) ', nm, ' ', res, ' ', err_stat))
+        }
+
+        # Compare models with the observed envelope 
+        model_x = data_store[[nm]][[ ii[1] ]]$x_k
+        model_max = data_store[[nm]][[ ii[1] ]]$max_stg_k
+        model_min = data_store[[nm]][[ ii[1] ]]$min_stg_k
+        data_upper = suppressWarnings(approx(d1[,1], d1[,2], xout=model_x )$y) # Ties --> warnings
+        data_lower = suppressWarnings(approx(d1[,1], d1[,3], xout=model_x )$y)
+        
+        # Only use values in the range of the data
+        dr = which((model_x > min(d1[,1])) & (model_x < max(d1[,1])))
+
+        # Check there are enough values
+        if(any(is.na(data_upper[dr]) | is.na(data_lower[dr]))){
+            print(paste0('FAIL (problem interpolating data) ', nm, ' ', res))
+        }else{
+            print('PASS')
+        }
+        
+        err_stat_upper = mean(abs(data_upper - model_max)[dr]) 
+        err_stat_lower = mean(abs(data_lower - model_min)[dr]) 
+
+        # Test with ad-hoc error thresholds
+        if(err_stat_upper < 0.013){
+            print('PASS')
+        }else{
+            print(paste0('FAIL (err_stat_upper) ', err_stat_upper, ' ', nm, ' ', res))
+        }
+
+        if(err_stat_lower < 0.005){
+            print('PASS')
+        }else{
+            print(paste0('FAIL (err_stat_lower)', err_stat_lower, ' ', nm, ' ', res))
+        }
+    }
+
+    ## Add in a PASS/FAIL test by comparison with Madsen
+    #test_xrange = seq(21250, 21600, by=10) # Leading part of bore
+    #test_SWALS = approx(x, stage, xout=test_xrange)$y # Due to plot ordering, this should be the highest-res solution
+    #test_Madsen = approx(madsen_2008_bore[,1], madsen_2008_bore[,2], xout=test_xrange)$y
+
+    ## Test 1 -- compare average abs deviation with Madsen.
+    #test_stat = mean(abs(test_SWALS - test_Madsen))
+    #err_tol = 0.3 # Ad-hoc choice
+    ##print(test_stat)
+    #if(test_stat < err_tol){
+    #    print('PASS')
+    #}else{
+    #    print(paste0('FAIL', nm, test_stat))
+    #}
+
+    ## Test 2 -- compare maxima with madsen
+    #test_stat_2 = max(test_SWALS) - max(test_Madsen)
+    #err_tol_2 = 0.3 # Ad-hoc choice
+    #if(abs(test_stat_2) < err_tol_2){
+    #    print('PASS')
+    #}else{
+    #    print(paste0('FAIL', nm, test_stat, test_stat_2))
+    #}
+}
---
+++ propagation/SWALS/examples/dispersive/shoaling_variable_seabed/shoaling_variable_seabed.f90
@@ -0,0 +1,174 @@
+module local_routines 
+    !!
+    !! Coulaud et al (2025) http://dx.doi.org/10.1016/j.coastaleng.2024.104645
+    !! Case 2: Unsteady shoaling of a wave train
+    !!
+
+    use global_mod, only: dp, ip, charlen, pi, gravity
+    use domain_mod, only: domain_type, STG, UH, VH, ELV
+    implicit none
+
+    ! Variables defined in paper
+    integer(ip), parameter :: N = 10
+    real(dp), parameter :: d1 = 1.0_dp, &
+        a1 = 0.125_dp * d1, &
+        dmin = 0.2_dp * d1, &
+        L = 50.0_dp * d1, &
+        xl = 0.0_dp, &
+        xr = 74.0_dp * d1
+
+    contains
+
+    subroutine set_initial_conditions(domain, long_dimension)
+        type(domain_type), intent(inout):: domain
+        character(len=charlen), intent(in) :: long_dimension
+
+        real(dp):: x, y, elev
+        integer(ip):: j, i, k
+        real(dp):: gauge_xy(2,11), wall
+
+        domain%msl_linear = 0.0_dp
+        domain%U(:,:,UH:VH) = 0.0_dp
+
+        if(long_dimension == 'y') then
+            ! Initial elevation
+            do i = 1, domain%nx(1)
+                where(domain%y <= L) domain%U(i,:,ELV) = -(dmin/d1 + (1.0_dp - dmin/d1)/cosh(tan((pi*domain%y/(2*L)))))
+                where(domain%y > L) domain%U(i,:,ELV) = -(dmin/d1)
+            end do
+
+            ! Initial stage
+            do i = 1, domain%nx(1)
+                ! Plot from the paper makes clear this is only in the left part of the domain
+                where(domain%y <= L) &
+                    domain%U(i,:,STG) = d1 * (a1/d1)*(cos(2.0_dp*pi*N*domain%y/L)/cosh(tan(pi*domain%y/(2*L))))
+            end do
+
+        else if(long_dimension == 'x') then
+            ! Initial elevation
+            do j = 1, domain%nx(2)
+                where(domain%x <= L) domain%U(:,j,ELV) = -(dmin/d1 + (1.0_dp - dmin/d1)/cosh(tan((pi*domain%x/(2*L)))))
+                where(domain%x > L) domain%U(:,j,ELV) = -(dmin/d1)
+            end do
+
+            ! Initial stage
+            do j = 1, domain%nx(2)
+                ! Plot from the paper makes clear this is only in the left part of the domain
+                where(domain%x <= L) &
+                    domain%U(:,j,STG) = d1 * (a1/d1)*(cos(2.0_dp*pi*N*domain%x/L)/cosh(tan(pi*domain%x/(2*L))))
+            end do
+        else
+            stop "unknown long dimension"
+        end if
+
+        ! Reflective boundaries on 4 sides
+        wall = 1.0_dp
+        domain%U(:, 1:2, ELV) = wall
+        domain%U(1:2, :, ELV) = wall
+        domain%U(:, domain%nx(2)-1:domain%nx(2), ELV) = wall
+        domain%U(domain%nx(1)-1:domain%nx(1), :, ELV) = wall
+   
+        ! Stage >= bed 
+        domain%U(:,:,STG) = max(domain%U(:,:,STG), domain%U(:,:,ELV))
+        
+    end subroutine
+
+end module 
+
+!@!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+program shoaling_variable_seabed
+    !!
+    !! Coulaud et al (2025) http://dx.doi.org/10.1016/j.coastaleng.2024.104645
+    !! Case 2: Unsteady shoaling of a wave train
+    !!
+    use global_mod, only: ip, dp
+    use multidomain_mod, only: multidomain_type
+    use boundary_mod, only: boundary_stage_transmissive_normal_momentum
+    use local_routines
+    implicit none
+
+    type(multidomain_type):: md
+
+    ! Approx timestep between outputs
+    real(dp), parameter :: approximate_writeout_frequency = 0.1_dp
+    real(dp), parameter :: final_time = 45.0_dp*sqrt(d1/gravity)
+
+    ! Domain info
+    character(charlen) :: timestepping_method, buf, long_dimension
+
+    ! Local variables 
+    real(dp) :: dx_in, dx, timestep
+
+    ! Timestepping method
+    call get_command_argument(1, timestepping_method)  
+    ! Resolution
+    call get_command_argument(2, buf)
+    read(buf, *) dx_in
+
+    ! Set the long-dimension (either 'y' or 'x'). Use this run in 1D along x or y axes
+    call get_command_argument(3, long_dimension) 
+      
+    
+    ! Resolution
+    dx = d1*dx_in 
+
+    ! Setup model with 1 domain
+    allocate(md%domains(1))
+
+    if(long_dimension == 'y') then
+        md%domains(1)%lw = [5*dx, xr + 2*dx]
+        md%domains(1)%lower_left =  [-2.5_dp * dx, -2.5*dx] ! Real flow over x==0, y >= -(dx/2) (with space for reflective boundaries)
+    else if(long_dimension == 'x') then
+        md%domains(1)%lw = [xr + 2*dx, 5*dx]
+        md%domains(1)%lower_left =  [-2.5_dp * dx, -2.5*dx] ! Real flow over y==0, x >= -(dx/2) (with space for reflective boundaries)
+    else
+        stop "unknown long_dimension"
+    end if
+
+    md%domains(1)%nx = nint(md%domains(1)%lw/dx)
+    md%domains(1)%timestepping_method = timestepping_method
+    md%domains(1)%use_dispersion = .true. !
+    !md%domains(1)%nc_grid_output%flush_every_n_output_steps = 1_ip !
+
+    md%domains(1)%nontemporal_grids_to_store = [character(len=charlen):: 'max_stage', 'min_stage', 'elevation0']
+
+    ! Tapering off of dispersive terms (won't have any effect)
+    md%domains(1)%ds%td1 = 0.01_dp
+    md%domains(1)%ds%td2 = 0.005_dp
+    !md%domains(1)%ds%tridiagonal_inner_iter = 4_ip
+
+    ! Non-TVD limiting for finite volume schemes
+    md%domains(1)%theta = 4.0_dp
+
+    ! Output directory should record the solver and resolution
+    md%output_basedir = 'OUTPUTS/' // trim(timestepping_method) // &
+        '_long_dimension_' // trim(long_dimension) // '_' // trim(buf)
+
+    call md%setup
+
+    call set_initial_conditions(md%domains(1), long_dimension)
+
+    call md%make_initial_conditions_consistent() ! Get the initial volume right
+
+    ! Fixed timestep  
+    timestep = md%stationary_timestep_max() * 0.8_dp 
+    !print*, trim(timestepping_method), ', dx = ', dx, ', timestep = ', timestep
+
+    ! Evolve the code
+    do while (.true.)
+
+        ! Avoid storing grids often
+        call md%write_outputs_and_print_statistics(&
+            approximate_writeout_frequency=approximate_writeout_frequency, &
+            write_grids_less_often = 1_ip)
+
+        if (md%domains(1)%time > final_time) exit
+
+        call md%evolve_one_step(timestep)
+
+    end do
+
+    call md%finalise_and_print_timers
+
+end program
