223b134cab197242d45bba91ed8cb1db1c628ca7
matt <matthew.macaulay@ga.gov.au>
2025-11-20 12:51:54 +1100

    Choose and run scenarios for animation

---
+++ misc/gladstone_2024_2025/animation/choose_scenario/plot_scenario.R
@@ -0,0 +1,20 @@
+#' Quick plot of globbed scenarios' max_stage.
+#' Works with tarred files too
+library(terra)
+
+
+md_dirs <- Sys.glob("/g/data/w85/tsunami/MODELS/inundation/QLD_tsunami_inundation_QFES/Gladstone_2024/swals/OUTPUTS/ptha/sea_level_vary/random_kermadectonga2/ptha18_random_scenarios_kermadectonga2_row_*_Mw_94_HS-full-ambient_sea_level_0")
+
+for (md_dir in md_dirs) {
+    tif <- paste("/vsitar", md_dir, "raster_output_files.tar", "max_stage_domain_98.tif", sep = "/")
+
+    print(tif)
+    r <- rast(tif)
+
+    fn <- paste0(basename(md_dir), ".png")
+    filename <- file.path("plot_max_stage", "domain_98", fn)
+    dir.create(dirname(filename), showWarnings = FALSE, recursive = TRUE)
+    png(filename = filename)
+    plot(r, range = c(0, 1))
+    dev.off()
+}
