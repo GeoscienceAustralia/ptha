d86e066cbac6f37ff1e71602ffaa34c4fd520c2e
gareth-d-ga <gareth.davies@ga.gov.au>
2026-02-17 18:25:57 +1100

    adding nonsymmetric version of radial potential test problem

---
+++ propagation/SWALS/examples/dispersive/potential_solution_nonsymmetric/potential_solution.R
@@ -0,0 +1,151 @@
+#
+# Solve the potential flow equation for the free surface
+# Equation 22 from 
+#     Saito, T. Dynamic tsunami generation due to sea-bottom deformation: 
+#     Analytical representation based on linear potential theory Earth Planets Space, 2013, 65, 1411-1423
+#
+
+# Domain = Box with side-length L
+L = 200000 
+g = 9.8
+h0 = 4000 # Depth -- uniform
+N = 801
+
+xs = seq(-L/2, L/2, len=N)
+ys = seq(-L/2, L/2, len=N)
+
+dxs = diff(range(xs))/(N-1)
+dys = diff(range(ys))/(N-1)
+
+## Lon/lat corresponding to origin of xs/ys
+#central_lon = 0
+#central_lat = 45
+#radius_earth = 6371000
+#ys_lat = ys/(2*pi*radius_earth)*360 + central_lat
+#xs_lon = xs/(2*pi*radius_earth*cos(central_lat/180*pi))*360 + central_lon
+
+# Instantaneous displacement - NON-SYMMETRIC (elliptical/asymmetric form)
+# Use different scales in x and y directions to break radial symmetry
+d = matrix(0, ncol=length(xs), nrow=length(ys))
+for(j in 1:ncol(d)){
+    # Elliptical Gaussian with different standard deviations in x and y
+    # Plus an asymmetric oscillatory component
+    x_dist = xs - 0
+    y_dist = ys[j] - 0
+
+    # Elliptical component: different scales in x and y directions
+    elliptical_term = exp(-0.5 * ((x_dist/(3*h0))**2 + (y_dist/(5*h0))**2)) * 2.0
+
+    # Asymmetric oscillatory term (depends on x and y independently)
+    r = sqrt(x_dist**2 + y_dist**2)
+    oscillatory_term = 0.3 * sin(2*pi*x_dist/(8*h0)) * cos(2*pi*y_dist/(10*h0)) * (r < 16*h0)
+
+    d[,j] = elliptical_term + oscillatory_term
+}
+
+# Wavenumbers
+kx = 2*pi*pmin(0:(N-1), N - (0:(N-1)) )/L
+ky = 2*pi*pmin(0:(N-1), N - (0:(N-1)) )/L
+
+# k = sqrt(kx^2 + ky^2)
+k = d*0
+for(j in 1:ncol(k)){
+    k[,j] = sqrt(kx**2 + ky[j]**2)
+}
+
+w0 = sqrt(g * k * tanh(k * h0))
+
+free_surface_solution<-function(t){
+    # Eqn 25 of Saito (2013) when H(t) = 1 and d(delta(t))/dt = 0
+    # (i.e. away from the generation zone, after the rise time)
+
+    if(t > 0){
+        d_fft = fft(d)
+        inv_fft = 1/prod(dim(d)) * Re( fft(d_fft/cosh(k*h0) * cos(w0*t), inverse=TRUE) )
+    }else{
+        stop('Not implemented for t <= 0')
+    }
+    return(inv_fft)
+}
+
+pressure_residual_bed_solution<-function(t, rho0 = 1024){
+    # Eqn 25 of Saito (2013) when H(t) = 1 and d(delta(t))/dt = 0
+    # (i.e. away from the generation zone, after the rise time)
+    #
+    # Note that to get the wave-height from the pressure oscillation, we should
+    # multiply by 'cosh(k * h0) / (rho * g)'. This approaches the usual adjustment
+    # (1/(rho * g)) for sufficiently small (k*h0), but is nearly 2x greater for a
+    # wave-period of 2min in a depth of 4000m. 
+    # library(rptha)
+    # H0 = 4000
+    # cosh(2*pi * H0/airy_wavelength(period=seq(120, 600, by=60), h=H0, g=9.81))
+    #[1] 1.967542 1.310377 1.157754 1.096538 1.065464 1.047419 1.035976 1.028249
+    #[9] 1.022780
+    ## The effect is weaker in shallower water, as expected
+    # H0 = 2000
+    # cosh(2*pi * H0/airy_wavelength(period=seq(120, 600, by=60), h=H0, g=9.81))
+    # [1] 1.359862 1.138280 1.074144 1.046435 1.031873 1.023254 1.017724 1.013961
+    # [9] 1.011284
+    #
+    # At sites like NZJ which show clear late dispersive waves from Hunga-Tonga, and
+    # around 14 waves per hour in 2 km depth, the effect is likely small:
+    # H0 = 2000
+    # cosh(2*pi*H0/airy_wavelength(period=3600/14, h=H0, g=9.81))
+    # # [1] 1.064086
+    #
+    # But at a site like NZG, in about 6000m depth, and also about 14 waves/hr, the effect
+    # is becoming significant.
+    # H0 = 6000
+    # cosh(2*pi*H0/airy_wavelength(period=3600/14, h=H0, g=9.81))
+    #[1] 1.214376
+
+
+
+
+    if(t > 0){
+        d_fft = fft(d)
+        inv_fft = rho0 * g/prod(dim(d)) * Re( fft(d_fft/(cosh(k*h0)**2) * cos(w0*t), inverse=TRUE) )
+    }else{
+        stop('Not implemented for t <= 0')
+    }
+    return(inv_fft)
+}
+
+
+# Solution for very small time
+sol0 = free_surface_solution(0.0001)
+
+# # Later times -- do not let the wave exit the boundaries
+# maxT = 1500
+# dT = 30
+# Nt = round(maxT/dT)
+# sols = array(NA, dim=c(dim(sol0), Nt))
+# for(i in 0:(Nt-1)){
+#     if(i == 0){
+#         sols[,,1] = sol0
+#     }else{
+#         sols[,,i+1] = solution(i*dT)
+#     }
+# }
+
+#
+suppressMessages(library(raster))
+r1 = raster(sol0, xmn=min(xs)-dxs/2, xmx=max(xs)+dxs/2, ymn=min(ys)-dys/2, ymx=max(ys)+dys/2)
+writeRaster(r1, file='initial_condition_file.tif', overwrite=TRUE, options=c('COMPRESS=DEFLATE'))
+
+make_final_solution<-function(comptime){
+    # Solution to compare against
+    # Ensure the solution at the end time is small enough that the flow has not reached
+    # the boundary -- because these analytical calcuations are using periodic boundary
+    # conditions, whereas SWALS will not be.
+    #comptime = 100 
+    solend = free_surface_solution(comptime)
+
+    #r1 = raster(solend, xmn=min(xs), xmx=max(xs), ymn=min(ys), ymx=max(ys))
+    #writeRaster(r1, file=paste0('solution_time_', comptime, '.tif'), overwrite=TRUE, options=c('COMPRESS=DEFLATE'))
+    return(solend)
+}
+
+# The 'raster' function interprets the orientation differently to R's image
+# function, and the following can account for that.
+flip_like_raster<-function(mat){ a = t(mat); b = a[,ncol(a):1]; b}
---
+++ propagation/SWALS/examples/dispersive/potential_solution_nonsymmetric/radial_potential_nested.f90
@@ -0,0 +1,165 @@
+module local_routines 
+    !!
+    !! Setup a radially nonsymmetric problem with potential flow
+    !!
+    use global_mod, only: dp, ip, charlen
+    use domain_mod, only: domain_type, STG, UH, VH, ELV
+    use read_raster_mod, only: multi_raster_type
+    implicit none
+
+    real(dp), parameter :: bed_elev = -4000.0_dp
+
+    contains 
+
+    subroutine set_initial_conditions_nonsymmetric_radial_potential(domain)
+        !
+        ! This function sets the initial conditions in a domain
+        !
+        class(domain_type), target, intent(inout):: domain
+
+        type(multi_raster_type):: stage_data
+        integer(ip) :: i, j
+        real(dp), allocatable :: x(:), y(:)
+
+        ! Elevation (flat bed)
+        domain%U(:,:,ELV) = bed_elev 
+
+        ! Initial fluxes are zero
+        domain%U(:,:,UH:VH) = 0.0_dp
+
+        ! Stage from a file
+        call stage_data%initialise([character(len=charlen):: 'initial_condition_file.tif'])
+        allocate(x(domain%nx(1)), y(domain%nx(1)))
+        x = domain%x
+        do j = 1, domain%nx(2)
+            y = domain%y(j)
+            call stage_data%get_xy(x,y, domain%U(:,j,STG), domain%nx(1), bilinear=1_ip)
+        end do
+
+    end subroutine
+
+
+end module 
+
+!@!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+program nonsymmetric_radial_potential
+    !!
+    !! Radially nonsymmetric problem for potential flow
+    !!
+    use global_mod, only: ip, dp, charlen, default_linear_timestepping_method, default_nonlinear_timestepping_method
+    use multidomain_mod, only: multidomain_type
+    use domain_mod, only: UH, VH
+    use boundary_mod, only: flather_boundary, transmissive_boundary
+    use logging_mod, only: log_output_unit
+    use local_routines
+    use coarray_intrinsic_alternatives, only: swals_mpi_init, swals_mpi_finalize
+    implicit none
+
+    integer(ip):: i, nsteps
+    real(dp):: last_write_time
+    type(multidomain_type) :: md
+
+    ! Approx timestep between outputs
+    real(dp), parameter :: approximate_writeout_frequency = 0.010_dp
+    real(dp), parameter :: final_time = 50.0_dp
+
+    real(dp), parameter :: mesh_refine = 4.0_dp
+
+    ! length/width
+    real(dp), parameter, dimension(2):: global_lw = 200000.0_dp * [1.0_dp, 1.0_dp]
+    ! lower-left corner coordinate
+    real(dp), parameter, dimension(2):: global_ll = -global_lw/2.0_dp
+    ! grid size (number of x/y cells)
+    integer(ip), parameter, dimension(2):: global_nx = nint([100, 50]*mesh_refine + 1) ! Deliberately uneven
+
+    integer(ip), parameter :: timestepping_refinement_factor = 1_ip, dx_refinement_factor = 3_ip
+    real(dp) :: global_dt = 401.0_dp/(global_nx(1) * timestepping_refinement_factor) * (4.0_dp/mesh_refine) !* (1.0_dp/3.0_dp)
+    integer(ip), parameter :: nested_timestepping_refinement_factor = dx_refinement_factor * timestepping_refinement_factor
+
+    !integer(ip), parameter :: mnlt = 40 ! Min nesting layer thickness:
+    real(dp), parameter :: nesting_layer_thick_on_depth = 2.5_dp
+
+    ! Misc
+    integer(ip) :: j, nd
+    character(len=charlen) :: buffer
+
+    call swals_mpi_init
+
+    ! Read load balance file as first commandline argument -- can be empty ""
+    ! in which case defaults will apply.
+    call get_command_argument(1, md%load_balance_file)
+   
+    ! Number of domains in model (1 for no nesting, 2 for nesting) 
+    call get_command_argument(2, buffer)
+    read(buffer, *) nd
+
+    allocate(md%domains(nd))
+
+    !
+    ! Set the domain properties
+    !
+    md%domains(1)%timestepping_method = 'midpoint' !default_linear_timestepping_method !'leapfrog_nonlinear' !'rk2' ! 
+
+    ! Domain Geometry
+    md%domains(1)%lw = global_lw
+    md%domains(1)%lower_left = global_ll
+    md%domains(1)%nx = global_nx
+    md%domains(1)%msl_linear = 0.0_dp
+    md%domains(1)%minimum_nesting_layer_thickness = nint(nesting_layer_thick_on_depth * (-bed_elev)/minval(global_lw/global_nx)) !mnlt
+    md%domains(1)%theta = 4.0_dp
+    md%domains(1)%timestepping_refinement_factor = timestepping_refinement_factor
+    md%domains(1)%use_dispersion = .true.
+    !md%domains(1)%nc_grid_output%flush_every_n_output_steps = 1_ip
+    md%domains(1)%boundary_subroutine => flather_boundary
+
+    if(nd > 1) then
+        ! Nested domain
+        call md%domains(2)%match_geometry_to_parent(&
+            parent_domain=md%domains(1), &
+            lower_left = global_ll + global_lw/4.0_dp, &
+            upper_right = global_ll + global_lw*(13.0_dp/24.0_dp), & ! Deliberately not symmetric
+            dx_refinement_factor = dx_refinement_factor, &
+            timestepping_refinement_factor = nested_timestepping_refinement_factor)
+        md%domains(2)%timestepping_method = 'midpoint' !default_nonlinear_timestepping_method !'rk2' !'leapfrog_nonlinear' !'linear'
+        md%domains(2)%theta = 4.0_dp
+        md%domains(2)%msl_linear = 0.0_dp
+        md%domains(2)%minimum_nesting_layer_thickness = nint(&
+            dx_refinement_factor*nesting_layer_thick_on_depth * (-bed_elev)/minval(global_lw/global_nx)) !mnlt
+        md%domains(2)%use_dispersion = .true.
+        !md%domains(2)%nc_grid_output%flush_every_n_output_steps = 1_ip
+        !md%domains(2)%boundary_subroutine => transmissive_boundary !flather_boundary
+    end if
+
+    ! Output variables to store
+    do j = 1, size(md%domains)
+        md%domains(j)%time_grids_to_store = [character(len=charlen):: 'stage', 'uh', 'vh']
+        md%domains(j)%nontemporal_grids_to_store = [character(len=charlen):: 'max_stage', 'max_speed', 'max_flux', &
+            'arrival_time', 'elevation0', 'manning_squared']
+    end do
+
+    call md%setup()
+
+    ! Set initial conditions
+    do j = 1, size(md%domains)
+        call set_initial_conditions_nonsymmetric_radial_potential(md%domains(j))
+    end do
+    call md%make_initial_conditions_consistent()
+
+    write(log_output_unit, *) 'Max timestep: ', md%stationary_timestep_max()
+
+    ! Evolve the solution
+    do while (.TRUE.)
+
+        call md%write_outputs_and_print_statistics(approximate_writeout_frequency=approximate_writeout_frequency)
+
+        if (md%domains(1)%time > final_time) exit
+
+        call md%evolve_one_step(global_dt)
+
+    end do
+
+    call md%finalise_and_print_timers
+    call swals_mpi_finalize
+
+end program
