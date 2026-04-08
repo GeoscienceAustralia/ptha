53c4d81728aaa0d8676445499cad4aff4410dfcb
gareth-d-ga <gareth.davies@ga.gov.au>
2026-04-08 14:32:39 +1000

    add post-process scripts

---
+++ misc/WA_2025_2027/kalbarri_2_onslow/swals/post_process/bzip2_some_files.R
@@ -0,0 +1,13 @@
+
+all_files = Sys.glob('../OUTPUTS/ptha18-kalbarri2onslow-hazar*/random_*/*/RUN*.tar')
+
+bzip2_file<-function(filename){
+    cmd = paste0('bzip2 ', filename)
+    #cmd = paste0('bzip2 -d ', filename)
+    result = system(cmd)
+    return(result)
+}
+
+library(parallel)
+
+mclapply(all_files, bzip2_file, mc.cores=48, mc.preschedule=FALSE)
